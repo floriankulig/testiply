898da91d0e8d7007702a43b8a72dac711dcefae0
Florian Kulig <florian.kulig@web.de>
2020-12-10 21:37:32 +0100

    init next for TS

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../scss/globals.scss'
+import { AppProps } from 'next/app'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
