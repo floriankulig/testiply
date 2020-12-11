99d702869f4c75293f7d519a9d3345149115eb46
Florian Kulig <florian.kulig@web.de>
2020-12-11 22:56:35 +0100

    added LandingPageHeader Component

---
+++ src/components/home/LandingPageHeader.tsx
@@ -0,0 +1,11 @@
+export const LandingPageHeader: React.FC = () => {
+    return (
+        <header className="container">
+            <div className="logo">BetaStore</div>
+            <ul className="nav-links">
+                <li>Log In</li>
+                <li>Sign Up</li>
+            </ul>
+        </header>
+    )
+}
