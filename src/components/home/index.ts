c8ba524f560836e89ec422a13844ea872470a1d5
Florian Kulig <florian.kulig@web.de>
2020-12-11 22:56:59 +0100

    managing file imports exports

---
+++ src/components/home/index.ts
@@ -0,0 +1,9 @@
+//Gather all Files needed for index.tsx page
+import { LandingPageHeader as Header } from "./LandingPageHeader";
+import { Hero } from "./Hero";
+import { Features } from "./Features";
+import { Benefits } from "./Benefits";
+import { InfoFooter as Footer } from "../InfoFooter";
+
+//Export Files for usage in pages/index.tsx document
+export { Header, Hero, Features, Benefits, Footer };
