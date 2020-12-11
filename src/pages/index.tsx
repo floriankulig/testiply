39674162947442fc5e41a97698f3e098c9e4dcd2
Florian Kulig <florian.kulig@web.de>
2020-12-11 22:55:53 +0100

    display new contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import Head from 'next/head'
+import { Header, Hero, Features, Benefits, Footer } from "components/home"
 
 const Home: React.FC = () => {
   return (
-    <div >
+    <div id="page--home">
       <Head>
         <title>Beta App Store</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
-      Home Page
+      <Header></Header>
+      <Hero></Hero>
+      {/*<Features></Features>
+      <Benefits></Benefits>
+      <Footer></Footer> */}
     </div>
   )
 }
