72877c84f22843049212b955a5626c28e8516721
Florian Kulig <florian.kulig@web.de>
2020-12-13 17:59:52 +0100

    using sc for LandingPageHeader

--- src/components/home/LandingPageHeader.tsx
+++ src/components/home/LandingPageHeader.tsx
@@ -1,11 +1,37 @@
+import styled from "styled-components"
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    color: white;
+
+    .logo {
+        font-size:1.8rem;
+        font-weight: bold;
+    }
+
+    .nav-links {
+        @media (min-width(1080px)) {
+        width: 10em;
+        }
+        width: 8em;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+  }
+`
+
+
 export const LandingPageHeader: React.FC = () => {
     return (
-        <header className="container">
+        <Header className="container">
             <div className="logo">BetaStore</div>
             <ul className="nav-links">
                 <li>Log In</li>
                 <li>Sign Up</li>
             </ul>
-        </header>
+        </Header>
     )
 }
