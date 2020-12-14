7307da77af21558c0281e28b2a96f6872152c8f8
Florian Kulig <florian.kulig@web.de>
2020-12-14 23:05:12 +0100

    Made ErrorMessage Component

---
+++ src/components/ErrorMessage.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ErrorMessage = styled.p`
+  color: red;
+  padding: 5px 10px;
+  border: 1px solid red;
+  border-radius: 5px;
+  width: max-content;
+  max-width: 100%;
+
+  svg {
+    width: 20px;
+    height: 20px;
+    transform: translateY(3px);
+    margin-right: 5px;
+  }
+`;
