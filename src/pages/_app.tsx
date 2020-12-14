66a557c11e8ad461d4e7c21870f41d5ed2c34293
Florian Kulig <florian.kulig@web.de>
2020-12-14 19:07:03 +0100

    Added fully to styled-components, not needing sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,65 @@
 import { AppProps } from 'next/app'
-import "scss/App.scss"
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+  }
+  *,
+  *:before,
+  *:after {
+    -webkit-box-sizing: inherit;
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    line-height: 1;
+    font-family: "Roboto", sans-serif;
+    overflow-x: hidden;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+  p {
+    font-size: 16px;
+    line-height: 1.4;
+  }
+
+  .container {
+    width: 90%;
+    // not using margin: 0 auto; here, because that would interfear with (Styled)Components with container class
+    // we only want to control the margin on the sides, therefore the centering of an Component
+    margin-left: auto;
+    margin-right: auto;
+
+    @media (min-width: 1021px) {
+      width: 70%;
+    }
+  }
+`;
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (<Component {...pageProps} />)
+  return (<>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
