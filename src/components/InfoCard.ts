90b9dff07aaae0b48d38657b255d8b74b6af6508
Florian Kulig <florian.kulig@web.de>
2020-12-15 16:25:21 +0100

    added InfoCard Component

---
+++ src/components/InfoCard.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const InfoCard = styled.div`
+  background: #0012ff;
+  padding: 1em;
+  border-radius: 10px;
+  margin-bottom: -5em;
+  width: 400px;
+  clip-path: circle(30px at calc(400px - 30px) 30px);
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    clip-path: circle(75%);
+    background: #00b6ff;
+    margin-bottom: 1em;
+  }
+
+  h1 {
+    color: white;
+    margin: 0;
+    width: min-content;
+  }
+
+  p {
+    color: white;
+    font-size: 0.8rem;
+    max-width: 95%;
+  }
+
+  span {
+    float: right;
+    color: white;
+    font-size: 30px;
+    font-weight: bold;
+    transition: color 0.5s;
+    position: relative;
+    margin-right: 2.5%;
+  }
+  &:hover span {
+    color: rgba(255, 255, 255, 0);
+  }
+`;
