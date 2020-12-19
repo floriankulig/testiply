e5555b9625c5a689dd00fb36ace8a11103e8a684
Florian Kulig <florian.kulig@web.de>
2020-12-19 20:49:19 +0100

    Added all logic for UIButton in own component

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,11 +1,66 @@
-import styled from "styled-components";
+import { cssVar, darken, rgba } from "polished";
+import styled, { css } from "styled-components";
 
-export const Button = styled.button`
-  background-color: white;
+interface ButtonProps {
+  basic?: boolean;
+  transparent?: boolean;
+  rounded?: boolean;
+  bold?: boolean;
+  disableElevation?: boolean;
+  color?: string;
+}
+
+export const Button = styled.button<ButtonProps>`
   border: none;
   padding: 0.8em 1.8em;
-  font-weight: bold;
   font-size: 1.1rem;
-  border-radius: 2em;
+  ${(p) => p.bold && "font-weight: bold"};
+  border-radius: ${(p) => (p.rounded ? "3em" : "var(--border-radius)")};
   cursor: pointer;
+  transition: 0.5s all;
+  ${(p) =>
+    !p.disableElevation &&
+    css`
+      box-shadow: var(--btn-shadow);
+      &:hover {
+        box-shadow: var(--btn-shadow-hover);
+      }
+    `}
+
+  ${(p) =>
+    p.transparent || p.basic //When Button is transparent or basic
+      ? css`
+          background-color: transparent;
+          color: ${p.color ? p.color : "var(--primary)"};
+          box-shadow: none;
+          border: 1px solid
+            ${p.basic ? (p.color ? p.color : "var(--primary)") : "none"};
+
+          &:hover {
+            background-color: ${p.color
+              ? rgba(p.color, 0.1)
+              : rgba(p.theme.primary, 0.1)};
+            box-shadow: none;
+          }
+        `
+      : css`
+          //default styles (normal primary color)
+          background-color: ${p.color ? p.color : "var(--primary)"};
+          color: white;
+          &:hover {
+            background-color: ${p.color
+              ? darken(0.05, p.color)
+              : darken(0.05, p.theme.primary)};
+          }
+        `};
+
+  &:disabled {
+    cursor: default;
+    background-color: ${rgba(0, 0, 0, 0.12)};
+    color: rgba(0, 0, 0, 0.26);
+    box-shadow: none;
+    &:hover {
+      box-shadow: none;
+    }
+  }
 `;
