00365e62c1a56266dcc4fbdf532bbe2800d0c649
Florian Kulig <florian.kulig@web.de>
2020-12-22 22:34:40 +0100

    added easing function for animations

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -9,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
     --border-radius: ${theme.borderRadius};
     --sidebar-width: ${theme.sidebarWidth};
     --header-height: ${theme.headerHeight};
+    --easing: cubic-bezier(0.645, 0.045, 0.355, 1);
     --btn-shadow: ${rgba(0, 0, 0, 0.2)} 0px 3px 1px -2px,
       ${rgba(0, 0, 0, 0.14)} 0px 2px 2px 0px,
       ${rgba(0, 0, 0, 0.12)} 0px 1px 5px 0px; 
