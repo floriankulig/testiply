fb0aa60941ed73b4e87de795d9e45d86ce9c7fdc
Florian Kulig <florian.kulig@web.de>
2020-12-27 22:00:48 +0100

    removed query for link to store

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -68,7 +68,7 @@ export const Hero: React.FC = () => {
             <Content>
                 <h1>Test Apps.</h1>
                 <h1>Give Feedback.</h1>
-                <Link href="/store?tab=today">
+                <Link href="/store">
                     <Button rounded bold color="white">View Apps</Button>
                 </Link>
             </Content>
