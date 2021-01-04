4b5f8ecf58f656811f69be0e5168c6f8d3b5da9f
Florian Kulig <florian.kulig@web.de>
2021-01-04 23:20:18 +0100

    added TabHeader to apps tab

--- src/pages/store/apps.tsx
+++ src/pages/store/apps.tsx
@@ -1,4 +1,5 @@
 import { Layout } from 'components/layouts'
+import { TabHeader } from 'components/TabHeader'
 import { useSelectedTabValue } from 'context'
 import Head from 'next/head'
 import { useEffect } from 'react'
@@ -15,7 +16,9 @@ const Apps = () => {
             <Head>
                 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
             </Head>
-            Apps
+            <TabHeader>
+                Apps
+            </TabHeader>
         </>
     )
 }
