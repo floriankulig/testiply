1bfa97b28888e5fe1a9b0533de0fb6d89c129ed7
Florian Kulig <florian.kulig@web.de>
2021-01-09 17:55:50 +0100

    added basic AuthLayout

---
+++ src/components/layouts/AuthLayout.tsx
@@ -0,0 +1,30 @@
+import { AuthInfoSidebar } from "components/auth"
+import styled from "styled-components"
+import { FormType } from "ts"
+
+const StyledAuthLayout = styled.div`
+    max-height: 100vh;
+    height: 100vh;
+    display: flex;
+    justify-content: space-between;
+`;
+
+
+interface AuthLayoutProps {
+    children: React.ReactNode;
+    formType: FormType;
+}
+
+export const AuthLayout: React.FC<AuthLayoutProps> = ({ children, formType }) => {
+    return (
+        <StyledAuthLayout style={{ maxHeight: "100vh", height: "100vh" }}>
+            {formType === "register" && (
+                <AuthInfoSidebar type={formType} />
+            )}
+            {children}
+            {formType === "login" && (
+                <AuthInfoSidebar type={formType} />
+            )}
+        </StyledAuthLayout>
+    )
+}
--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -1,3 +1,4 @@
-import { Layout } from "./Layout";
+import { Layout } from './Layout';
+import { AuthLayout } from './AuthLayout';
 
-export { Layout };
+export { Layout, AuthLayout };
