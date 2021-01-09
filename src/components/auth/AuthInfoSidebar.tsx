f3cfc847e7c5f64400793c681238183839c97fc9
Florian Kulig <florian.kulig@web.de>
2021-01-09 19:33:22 +0100

    added section to change formtype

--- src/components/auth/AuthInfoSidebar.tsx
+++ src/components/auth/AuthInfoSidebar.tsx
@@ -2,15 +2,25 @@ import styled from "styled-components";
 import { FormType } from "ts"
 import { getTextColor } from "helpers";
 import { Policies, Socials } from "components/InfoFooter";
+import { Button } from "components/Button";
+import Link from "next/link";
 
 const StyledAuthInfoSidebar = styled.div`
-    width: 30vw;
+    width: var(--auth-sidebar-width);
     height: 100vh;
     background-color: #6B1EF1;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    padding: 3em;
+    padding: 15em 0 3em;
+
+    .cta-change-formtype{
+        h2{
+            color: ${getTextColor("#6B1EF1")};
+            margin-bottom: 1.5em;
+        }
+        text-align: center;
+    }
 `;
 
 const Info = styled.div`
@@ -34,7 +44,24 @@ interface AuthInfoSidebarProps {
 export const AuthInfoSidebar: React.FC<AuthInfoSidebarProps> = ({ type }) => {
     return (
         <StyledAuthInfoSidebar>
-            {type}
+            <div className="cta-change-formtype">
+                {type === "register" ? (
+                    <>
+                        <h2>Already have an Account?</h2>
+                        <Link href="/login">
+                            <Button color="white" big bold >Go to Log In</Button>
+                        </Link>
+                    </>
+                ) : (
+                        <>
+                            <h2>Don't have an account yet?</h2>
+                            <Link href="/register">
+                                <Button color="white" big bold>Go to Register</Button>
+                            </Link>
+                        </>
+                    )
+                }
+            </div>
             <Info>
                 <div className="info-group">
                     <Socials />
