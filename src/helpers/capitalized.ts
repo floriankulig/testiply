dd8025d01e6755dfe405fc9a2ed2abc5b9982c32
Florian Kulig <florian.kulig@web.de>
2021-01-10 22:41:37 +0100

    added helper function to capitalize strings

---
+++ src/helpers/capitalized.ts
@@ -0,0 +1,5 @@
+const capitalized = (word: string) => {
+  return word[0].toUpperCase() + word.slice(1);
+};
+
+export default capitalized;
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
-export { default as evaluateEmailRes } from "./evaluateEmailRes";
-export { default as getTextColor } from "./getTextColor";
+export { default as evaluateEmailRes } from './evaluateEmailRes';
+export { default as getTextColor } from './getTextColor';
+export { default as capitalized } from './capitalized';
