3aa96268e7c3f873fb70940c9b212b250c6e0e1b
Florian Kulig <florian.kulig@web.de>
2021-01-12 18:49:28 +0100

    using standard naming for Styled Components

--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-const FeaturesContainer = styled("ul")`
+const FeaturesContainer = styled.ul`
     display: flex;
     justify-content: space-between;
     flex-direction: column;
