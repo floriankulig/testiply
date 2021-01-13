1e0eab10106217002583f2754ae7cf61d6835912
Florian Kulig <florian.kulig@web.de>
2021-01-13 23:13:38 +0100

    importing styled components from new file

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,65 +1,16 @@
 import { Button } from "components/Button";
+import { Container, Form } from "./FormComponents"
 import { FormInput, SVGWrapper, TextField } from "components/FormInput";
 import { SelectionInput } from "components/SelectionInput";
-import { rgba } from "polished";
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
-import styled from "styled-components"
 import { FormType, Gender, genders } from "ts"
 import { DateInput } from "./DateInput";
 import { useIsMobile } from "hooks";
 import Link from "next/link";
 
-export const Container = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center; 
-    justify-content: center; 
-    min-height: calc(100vh - var(--header-height));
-    @media (${({ theme }) => theme.bp.big}){
-        height: 100vh;
-        max-height: 100vh;
-        width: calc(100vw - var(--auth-sidebar-width));
-    }
-`;
 
-export const Form = styled.form`
-    min-height: 50vh;
-    width: clamp(1px, 90%, 650px);
-    border-radius: 4em;
-    padding: 4em 3em;
-    margin: 5em 0;
-    background-color: var(--layout-nav-background);
-    box-shadow: 10px 30px 80px ${p => rgba(p.theme.navy, 0.1)};
-    @media (${({ theme }) => theme.bp.small}){
-            padding: 4em 6em;
-    }
-
-    h1{
-        margin: 0;
-        margin-bottom: 1.5em;
-    }
-
-   .meta-inputs {
-        width: 100%;
-        // same as FormInputs
-        max-width: 600px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        @media (${({ theme }) => theme.bp.small}){
-            flex-direction: row;
-        }
-    }
-
-    ${Button} {
-        margin-top: 2em;
-        @media (${({ theme }) => theme.bp.small}){
-            margin-top: 3em;
-        }
-    }
-`;
 
 
 interface AuthFormProps {
