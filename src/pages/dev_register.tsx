e239fb9adb1c8057a83e78321527e9bc84d915fe
Florian Kulig <florian.kulig@web.de>
2021-01-13 23:15:08 +0100

    cleaned up import

--- src/pages/dev_register.tsx
+++ src/pages/dev_register.tsx
@@ -1,12 +1,8 @@
 import { DevRegisterForm } from "components/auth";
 import { AuthLayout } from "components/layouts";
-import { useIsMobile } from "hooks";
 import { NextPage } from "next";
-import { useState } from "react";
 
 const DevRegister: NextPage = () => {
-
-
     return (
         <AuthLayout formType="dev_register">
             <DevRegisterForm />
