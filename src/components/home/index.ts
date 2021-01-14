0a8452d00e30e097bb9739ea5390f8de995939c0
Florian Kulig <florian.kulig@web.de>
2021-01-14 17:38:28 +0100

    removed export for page header

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,10 +1,9 @@
 //Gather all Files needed for index.tsx page
-import { LandingPageHeader as Header } from "./LandingPageHeader";
-import { Hero } from "./Hero";
-import { Features } from "./Features";
-import { Benefits } from "./Benefits";
-import { NewsLetter } from "./NewsLetter";
-import { InfoFooter as Footer } from "../InfoFooter";
+import { Hero } from './Hero';
+import { Features } from './Features';
+import { Benefits } from './Benefits';
+import { NewsLetter } from './NewsLetter';
+import { InfoFooter as Footer } from '../InfoFooter';
 
 //Export Files for usage in pages/index.tsx document
-export { Header, Hero, Features, Benefits, NewsLetter, Footer };
+export { Hero, Features, Benefits, NewsLetter, Footer };
