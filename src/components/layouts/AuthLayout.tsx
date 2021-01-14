512363f7747c01e81dc28149aeea005fbbb7110a
Florian Kulig <florian.kulig@web.de>
2021-01-14 18:39:51 +0100

    added support for new header

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -1,8 +1,10 @@
 import { AuthInfoSidebar } from "components/auth"
-import { AuthLayoutHeader } from "components/auth/AuthLayoutHeader";
 import { InfoFooter } from "components/InfoFooter";
+import { InfoPageHeader } from "components/InfoPageHeader";
+import { getTextColor } from "helpers";
 import { useIsMobile } from "hooks";
 import styled from "styled-components"
+import { theme } from "styles";
 import { FormType } from "ts"
 
 const StyledAuthLayout = styled.div`
@@ -15,15 +17,22 @@ const StyledAuthLayout = styled.div`
     }
 `;
 
-const Container = styled.div`
+const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+
+const FormContainer = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   min-height: calc(100vh - var(--header-height));
   @media (${({ theme }) => theme.bp.big}) {
-    height: 100vh;
-    max-height: 100vh;
+    /* height: 100vh;
+    max-height: 100vh; */
     width: calc(100vw - var(--auth-sidebar-width));
   }
 `;
@@ -40,13 +49,18 @@ export const AuthLayout: React.FC<AuthLayoutProps> = ({ children, formType }) =>
 
     return (
         <StyledAuthLayout style={{ minHeight: "100vh" }}>
-            <AuthLayoutHeader formType={formType} />
             {formType.includes("register") && !isMobile && (
                 <AuthInfoSidebar type={formType} />
             )}
-            <Container>
-                {children}
-            </Container>
+            <ContentContainer>
+                <InfoPageHeader style={{
+                    color: getTextColor(theme.layoutContentBg),
+                    padding: `0 ${isMobile ? "5%" : "4em"}`
+                }} />
+                <FormContainer>
+                    {children}
+                </FormContainer>
+            </ContentContainer>
             {formType === "login" && !isMobile && (
                 <AuthInfoSidebar type={formType} />
             )}
