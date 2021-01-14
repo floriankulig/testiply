8969348e1985f9676f1874af84bae31d06de7c19
Florian Kulig <florian.kulig@web.de>
2021-01-14 17:29:45 +0100

    reset dev register form

--- src/pages/dev/register.tsx
+++ src/pages/dev/register.tsx
@@ -1,11 +1,10 @@
-import { DevRegisterForm } from "components/auth";
 import { AuthLayout } from "components/layouts";
 import { NextPage } from "next";
 
 const DevRegister: NextPage = () => {
     return (
         <AuthLayout formType="dev_register">
-            <DevRegisterForm />
+            Hello
         </AuthLayout>
     )
 }
