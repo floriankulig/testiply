7d52163b78fe6d893eb9daa0bb2f92458b19de0c
Florian Kulig <florian.kulig@web.de>
2021-01-15 23:35:30 +0100

    rendering MultilevelForm for testing

--- src/pages/dev/register.tsx
+++ src/pages/dev/register.tsx
@@ -1,10 +1,15 @@
+import { MultilevelForm } from "components/forms";
 import { AuthLayout } from "components/layouts";
 import { NextPage } from "next";
+import { useState } from "react";
 
 const DevRegister: NextPage = () => {
+    const formNames = ["first", "second", "third", "fourth"]
+    const [activeForm, setActiveForm] = useState<string>(formNames[0]);
+
     return (
         <AuthLayout formType="dev_register">
-            Hello
+            <MultilevelForm setActiveForm={setActiveForm} formNames={formNames} />
         </AuthLayout>
     )
 }
