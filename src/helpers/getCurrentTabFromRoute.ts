1e78b9813c45de07b451a61d885428f980eb1169
Florian Kulig <florian.kulig@web.de>
2021-01-16 22:11:31 +0100

    added method for fethcing tab from route

---
+++ src/helpers/getCurrentTabFromRoute.ts
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { devTabNames, testerTabNames } from 'ts/constants';
+
+const getCurrentTabFromRoute = (): string => {
+  const router = useRouter();
+  const pathArray = router.pathname.split('/');
+  let tab: string;
+
+  console.log(pathArray);
+
+  if (!pathArray[2] && pathArray[1] === 'store') {
+    tab = 'today';
+  } else if (
+    testerTabNames.includes(pathArray[2]) ||
+    devTabNames.includes(pathArray[2])
+  ) {
+    tab = pathArray[2];
+  }
+
+  return tab;
+};
+
+export default getCurrentTabFromRoute;
