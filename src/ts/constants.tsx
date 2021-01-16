7d3062d8e41f882b44f2f8d77515a20013bc8574
Florian Kulig <florian.kulig@web.de>
2021-01-16 21:47:14 +0100

    added constants for sidebar tabs and their icons

---
+++ src/ts/constants.tsx
@@ -0,0 +1,31 @@
+import {
+  AiOutlineHome,
+  AiFillHome,
+  AiOutlineAppstore,
+  AiFillAppstore,
+  AiOutlineDashboard,
+  AiFillDashboard,
+} from 'react-icons/ai';
+import { FaComment, FaRegComment } from 'react-icons/fa';
+import {
+  IoGameControllerOutline,
+  IoGameController,
+  IoNewspaperOutline,
+  IoNewspaper,
+} from 'react-icons/io5';
+import { RiStackLine, RiStackFill } from 'react-icons/ri';
+
+export const testerTabNames = ['today', 'apps', 'games', 'categories', 'news'];
+export const testerTabIcons = [
+  [<AiOutlineHome />, <AiFillHome />],
+  [<AiOutlineAppstore />, <AiFillAppstore />],
+  [<IoGameControllerOutline />, <IoGameController />],
+  [<RiStackLine />, <RiStackFill />],
+  [<IoNewspaperOutline />, <IoNewspaper />]
+];
+export const devTabNames = ['dashboard', 'apps', 'feedback'];
+export const devTabIcons = [
+  [<AiOutlineDashboard />, <AiFillDashboard />],
+  [<AiOutlineAppstore />, <AiFillAppstore />],
+  [<FaRegComment />, <FaComment />]
+];
