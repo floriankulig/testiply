98ea0c390e9d2eb63d5098e07968bcdd904b1e2a
Florian Kulig <florian.kulig@web.de>
2021-01-16 21:46:54 +0100

    added dashboard route for dev dashboard

---
+++ src/pages/dev/dashboard.tsx
@@ -0,0 +1,28 @@
+import { Layout } from "components/layouts"
+import { TabHeader } from "components/TabHeader"
+import { useSelectedTabValue } from "context"
+import Head from "next/head"
+import { useEffect } from "react"
+
+const DevDashboard = () => {
+    const { setSelectedTab } = useSelectedTabValue()
+
+    useEffect(() => {
+        setSelectedTab("dashboard")
+    }, [])
+
+    return (
+        <>
+            <Head>
+                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            </Head>
+            <TabHeader>
+                Feedback
+            </TabHeader>
+        </>
+    )
+}
+DevDashboard.Layout = Layout
+
+
+export default DevDashboard
