3e7f873247d3fcf3691a5b2ed32565fd566daad4
Florian Kulig <florian.kulig@web.de>
2021-01-16 19:13:35 +0100

    Revert "setting no margin if there's no label"
    This reverts commit 582f6b3514e9e7827fa42468eb72a5bcd76efc3b.

--- src/components/SelectionInput.tsx
+++ src/components/SelectionInput.tsx
@@ -75,13 +75,7 @@ export const SelectionInput: React.FC<SelectionInputProps> = ({ label, style, cl
     }, []);
 
     return (
-        <FormInput
-            style={
-                !label ? { position: "relative", marginTop: 0, ...style }
-                    : { position: "relative", ...style }
-            }
-            className={className}
-        >
+        <FormInput style={{ position: "relative", ...style }} className={className}>
             {label && label}{optional && " (optional)"}
             <div ref={ref}>
                 <TextField>
