d6d23fbd07627f8d7b440b46be5fdccf81e76e39
Florian Kulig <florian.kulig@web.de>
2021-01-17 22:01:21 +0100

    using TextField component

--- src/components/auth/TesterAuthForm.tsx
+++ src/components/auth/TesterAuthForm.tsx
@@ -1,5 +1,5 @@
 import { Button } from "components/Button";
-import { FormInput, SVGWrapper, TextField } from "components/forms";
+import { AuthForm, FormInput, SVGWrapper, TextField, TextInput } from "components/forms";
 import { SelectionInput } from "components/SelectionInput";
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
@@ -9,45 +9,6 @@ import { DateInput } from "./DateInput";
 import { useIsMobile } from "hooks";
 import Link from "next/link";
 import { theme } from "styles";
-import styled from "styled-components";
-import { rgba } from "polished";
-
-const Form = styled.form`
-  width: clamp(1px, 90%, 650px);
-  border-radius: 4em;
-  padding: 4em 3em;
-  margin: 5em 0;
-  background-color: var(--layout-nav-background);
-  box-shadow: 10px 30px 80px ${(p) => rgba(p.theme.navy, 0.1)};
-  @media (${({ theme }) => theme.bp.small}) {
-    padding: 4em 6em;
-  }
-
-  h1 {
-    margin: 0;
-    margin-bottom: 1.5em;
-  }
-
-  .meta-inputs {
-    width: 100%;
-    // same as FormInputs
-    max-width: 600px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    @media (${({ theme }) => theme.bp.small}) {
-      flex-direction: row;
-    }
-  }
-
-  ${Button} {
-    margin-top: 2em;
-    @media (${({ theme }) => theme.bp.small}) {
-      margin-top: 3em;
-    }
-  }
-`;
-
 
 interface TesterAuthFormProps {
     formType: FormType;
@@ -70,67 +31,47 @@ export const TesterAuthForm: React.FC<TesterAuthFormProps> = ({ formType }) => {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        console.log("Submitted")
     }
 
     return (
         <>
-            <Form onSubmit={(e) => handleSubmit(e)}>
+            <AuthForm onSubmit={(e) => handleSubmit(e)}>
                 {formType === "register" ? (
                     <h1>Register</h1>
                 ) : (
                         <h1>Login</h1>
                     )}
-                <FormInput>
-                    Email
-                        <TextField>
-                        <input
-                            type="email"
-                            value={email}
-                            placeholder="E-Mail Address"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <SVGWrapper>
-                            <MdEmail />
-                        </SVGWrapper>
-                    </TextField>
-                </FormInput>
-                <FormInput>
+                <TextInput
+                    inputType="text"
+                    value={email}
+                    setValue={setEmail}
+                    svg={<MdEmail />}
+                    placeholder="Enter your E-Mail Address"
+                >
+                    E-Mail Address
+                </TextInput>
+                <TextInput
+                    inputType={showPasswords ? "text" : "password"}
+                    value={password}
+                    setValue={setPassword}
+                    svg={showPasswords ? <AiFillEyeInvisible /> : <AiFillEye />}
+                    placeholder="Must be at least 8 characters"
+                    svgClickHandler={() => setShowPasswords(prev => !prev)}
+                >
                     Password
-                        <TextField>
-                        <input
-                            type={showPasswords ? "text" : "password"}
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <SVGWrapper
-                            onClick={() => setShowPasswords(prev => !prev)}
-                            onKeyDown={() => setShowPasswords(prev => !prev)}
-                            clickable
-                        >
-                            {showPasswords ? <AiFillEyeInvisible /> : <AiFillEye />}
-                        </SVGWrapper>
-                    </TextField>
-                </FormInput>
+                </TextInput>
                 {formType === "register" && (<>
-                    <FormInput>
+                    <TextInput
+                        inputType={showPasswords ? "text" : "password"}
+                        value={confirmPassword}
+                        setValue={setConfirmPassword}
+                        svg={showPasswords ? <AiFillEyeInvisible /> : <AiFillEye />}
+                        placeholder="Must be at least 8 characters"
+                        svgClickHandler={() => setShowPasswords(prev => !prev)}
+                    >
                         Confirm Password
-                    <TextField>
-                            <input
-                                type={showPasswords ? "text" : "password"}
-                                placeholder="Confirm Password"
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                            />
-                            <SVGWrapper
-                                onClick={() => setShowPasswords(prev => !prev)}
-                                onKeyDown={() => setShowPasswords(prev => !prev)}
-                                clickable
-                            >
-                                {showPasswords ? <AiFillEyeInvisible /> : <AiFillEye />}
-                            </SVGWrapper>
-                        </TextField>
-                    </FormInput>
+                    </TextInput>
                     <div className="meta-inputs">
                         <DateInput label="Date Of Birth (optional)" style={inputsStacked ? { width: "100%" } : { width: "60%" }} minDate={minBirthDate} maxDate={maxBirthDate} date={dateOfBirth} setDate={setDateOfBirth} />
                         <SelectionInput label="Gender" style={inputsStacked ? { width: "100%" } : { width: "35%" }} selection={gender} setSelection={setGender} values={genders} optional />
@@ -138,7 +79,7 @@ export const TesterAuthForm: React.FC<TesterAuthFormProps> = ({ formType }) => {
                 </>
                 )}
                 <Button bold>{formType === "register" ? "Register" : "Log In"}</Button>
-            </Form>
+            </AuthForm>
             {isFullscreen && (
                 <div style={{ textAlign: "center" }}>
                     <h2>Want to publish your own apps?</h2>
