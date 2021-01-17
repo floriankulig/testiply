d9023428f8935e9bcb5a0869bfc03748d985eb9b
Florian Kulig <florian.kulig@web.de>
2021-01-17 22:06:38 +0100

    added basic styles/functionality

--- src/components/forms/MultilevelForm.tsx
+++ src/components/forms/MultilevelForm.tsx
@@ -1,14 +1,30 @@
+import { Button } from "components/Button";
 import { useEffect, useState } from "react"
+import styled from "styled-components";
+import { theme } from "styles";
+import { AuthForm } from "components/forms";
+
+const ToggleFormButtons = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    margin-top: 1em;
+    @media (min-width: 400px) {
+        flex-direction: row;
+    }
+`;
 
 interface MultilevelFormProps {
     className?: string;
     style?: Object;
+    children: React.ReactNode;
     formNames: string[];
     setActiveForm: React.Dispatch<React.SetStateAction<string>>;
     minLevel?: number;
+    handleSubmit: Function
 }
 
-export const MultilevelForm: React.FC<MultilevelFormProps> = ({ className, style, formNames, setActiveForm, minLevel }) => {
+export const MultilevelForm: React.FC<MultilevelFormProps> = ({ className, style, children, formNames, setActiveForm, minLevel, handleSubmit }) => {
     //the furthest the user can go back in the form; indexed at zero
     const minStep: number = minLevel || 0
     const [step, setStep] = useState<number>(minStep);
@@ -38,14 +54,40 @@ export const MultilevelForm: React.FC<MultilevelFormProps> = ({ className, style
 
     useEffect(() => {
         setActiveForm(formNames[step]);
-        console.log(minStep)
+        console.log(step)
     }, [step]);
 
     return (
-        <div className={className} style={style}>
-            Step: {step}; {formNames[step]}
-            <button onClick={() => handleFormChange("INCREMENT")}>in</button>
-            <button onClick={() => handleFormChange("DECREMENT")}>de</button>
-        </div>
+        <AuthForm onSubmit={(e) => handleSubmit(e)} className={className} style={style}>
+            {children}
+            <ToggleFormButtons>
+                <Button
+                    bold
+                    color={theme.layoutContentBg}
+                    disableElevation
+                    disabled={isAtLimit("MIN")}
+                    onClick={() => handleFormChange("DECREMENT")}
+                    onKeyDown={() => handleFormChange("DECREMENT")}
+                    type="button"
+                >
+                    Go Back
+                </Button>
+                {!isAtLimit("MAX") && (
+                    <Button
+                        bold
+                        onClick={() => handleFormChange("INCREMENT")}
+                        onKeyDown={() => handleFormChange("INCREMENT")}
+                        type="button"
+                    >
+                        Next Step
+                    </Button>
+                )}
+                {isAtLimit("MAX") && (
+                    <Button bold>
+                        Register
+                    </Button>
+                )}
+            </ToggleFormButtons>
+        </AuthForm>
     )
 }
