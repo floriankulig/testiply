20d091966ac99097f961ff5d87d64aeae6fbdceb
Florian Kulig <florian.kulig@web.de>
2021-01-17 22:03:43 +0100

    added correct SEO title in head for reg. screen

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,18 @@
 import { TesterAuthForm } from "components/auth";
 import { AuthLayout } from "components/layouts";
 import { NextPage } from "next";
+import Head from "next/head";
 
 const Register: NextPage = () => {
     return (
-        <AuthLayout formType="register" >
-            <TesterAuthForm formType="register" />
-        </AuthLayout>
+        <>
+            <Head>
+                <title>Register | Beta App Store</title>
+            </Head>
+            <AuthLayout formType="register" >
+                <TesterAuthForm formType="register" />
+            </AuthLayout>
+        </>
     )
 }
 
