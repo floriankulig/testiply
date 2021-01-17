5e1c3f3e631ef245e5dbdd39b7e5958a23add2cb
Florian Kulig <florian.kulig@web.de>
2021-01-17 22:02:06 +0100

    added imports/exports for new components

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,4 +1,14 @@
 import { SVGWrapper, FormInput, TextField, StyledFormInput } from './FormInput';
 import { MultilevelForm } from './MultilevelForm';
+import { AuthForm } from './AuthForm';
+import { TextInput } from './TextInput';
 
-export { SVGWrapper, FormInput, TextField, StyledFormInput, MultilevelForm };
+export {
+  SVGWrapper,
+  FormInput,
+  TextField,
+  StyledFormInput,
+  MultilevelForm,
+  AuthForm,
+  TextInput,
+};
