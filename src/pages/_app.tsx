aa8092c39fca4f827d4d7add568913fe04e4ea3d
Florian Kulig <florian.kulig@web.de>
2021-01-20 18:05:36 +0100

    linked to all new favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,15 @@ const MyApp = ({ Component, pageProps }: any) => {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+        <link rel="manifest" href="favicons/site.webmanifest" />
+        <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#6b1ef1" />
+        <link rel="shortcut icon" href="favicons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="msapplication-config" content="favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <ThemeProvider theme={theme}>
         <AuthProvider>
