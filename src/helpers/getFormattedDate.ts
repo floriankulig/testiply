ed086de40cd005b0a190d306074d0cec72caa09c
Florian Kulig <florian.kulig@web.de>
2021-01-27 16:54:25 +0100

    added function for formatting dates

---
+++ src/helpers/getFormattedDate.ts
@@ -0,0 +1,9 @@
+const getFormattedDate = (date: Date) => {
+  let year = date.getFullYear();
+  let month = (1 + date.getMonth()).toString().padStart(2, '0');
+  let day = date.getDate().toString().padStart(2, '0');
+
+  return month + '/' + day + '/' + year;
+};
+
+export default getFormattedDate;
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,3 +2,4 @@ export { default as evaluateEmailRes } from './evaluateEmailRes';
 export { default as getTextColor } from './getTextColor';
 export { default as capitalized } from './capitalized';
 export { default as getCurrentTabFromRoute } from './getCurrentTabFromRoute';
+export { default as getFormattedDate } from './getFormattedDate';
