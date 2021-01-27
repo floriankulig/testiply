010b190a53810945f4f77fd8f1752800215aa594
Florian Kulig <florian.kulig@web.de>
2021-01-28 00:07:26 +0100

    adjusted imports/exports

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,4 +2,5 @@ export { default as evaluateEmailRes } from './evaluateEmailRes';
 export { default as getTextColor } from './getTextColor';
 export { default as capitalized } from './capitalized';
 export { default as getCurrentTabFromRoute } from './getCurrentTabFromRoute';
+export { default as getPathname } from './getPathname';
 export { default as getFormattedDate } from './getFormattedDate';
