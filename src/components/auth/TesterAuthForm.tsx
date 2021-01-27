c0bf84a6106e8ef5b7baa50c7adf11487773da3e
Florian Kulig <florian.kulig@web.de>
2021-01-27 18:33:28 +0100

    added formik to tester auth forms

--- src/components/auth/TesterAuthForm.tsx
+++ src/components/auth/TesterAuthForm.tsx
@@ -1,11 +1,11 @@
 import { Button } from "components/Button";
-import { AuthForm, TextInput } from "components/forms";
-import { SelectionInput } from "components/SelectionInput";
+import { AuthForm, FormikDateInput, FormikSelectionInput, FormikTextInput } from "components/forms";
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
 import { FormType, Gender, genders } from "ts"
-import { DateInput } from "./DateInput";
+import { Form, Formik, FormikHelpers, FormikValues } from "formik"
+import * as Yup from 'yup';
 import { useIsMobile } from "hooks";
 import Link from "next/link";
 import { theme } from "styles";
@@ -14,39 +14,104 @@ interface TesterAuthFormProps {
     formType: FormType;
 }
 
+interface FormValues {
+    email: string;
+    password: string;
+    confirmPassword?: string;
+    dateOfBirth?: string;
+    gender?: Gender | null;
+}
+
+
 export const TesterAuthForm: React.FC<TesterAuthFormProps> = ({ formType }) => {
     const [showPasswords, setShowPasswords] = useState<boolean>(false);
-    const [gender, setGender] = useState<Gender | null>(null);
 
     const minBirthDate = new Date("1900-01-01")
     let maxBirthDate = new Date()
     maxBirthDate.setFullYear(maxBirthDate.getFullYear() - 10)
-    const [dateOfBirth, setDateOfBirth] = useState<Date>(maxBirthDate);
 
     const inputsStacked = useIsMobile(720);
     const isFullscreen = useIsMobile(1200);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("Submitted")
+    const initialValues: FormValues = formType === "login" ? {
+        email: "",
+        password: "",
+    } : {
+            email: "",
+            password: "",
+            confirmPassword: "",
+            dateOfBirth: "",
+            gender: null,
+        }
+
+    const validationSchema = Yup.object({
+        email: Yup.string().email('Invalid format').required('Required').min(3),
+        password: Yup.string().required('Required').min(8, "Has to be at least 8 characters"),
+        confirmPassword: (formType === "register" ? Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Passwords must match').required('Required') : undefined)
+    })
+
+    const handleSubmit = (values: FormikValues, helpers: FormikHelpers<FormikValues>) => {
+        console.log(values)
     }
 
     return (
         <>
             <AuthForm>
-                {formType === "register" ? (
-                    <h1>Register</h1>
-                ) : (
-                        <h1>Login</h1>
-                    )}
-                {formType === "register" && (<>
-                    <div className="meta-inputs">
-                        <DateInput label="Date Of Birth (optional)" style={inputsStacked ? { width: "100%" } : { width: "60%" }} minDate={minBirthDate} maxDate={maxBirthDate} date={dateOfBirth} setDate={setDateOfBirth} />
-                        <SelectionInput label="Gender" style={inputsStacked ? { width: "100%" } : { width: "35%" }} selection={gender} setSelection={setGender} values={genders} optional />
-                    </div>
-                </>
-                )}
-                <Button bold>{formType === "register" ? "Register" : "Log In"}</Button>
+                <h1>{formType === "register" ?
+                    "Register" : "Login"}
+                </h1>
+                <Formik
+                    initialValues={initialValues}
+                    onSubmit={async (values, helpers) => await handleSubmit(values, helpers)}
+                    validationSchema={validationSchema}
+                >
+                    <Form autoComplete="off">
+                        <FormikTextInput
+                            name="email"
+                            svg={<MdEmail />}
+                            label="E-Mail Address"
+                            placeholder="Enter your E-Mail Address"
+                        />
+                        <FormikTextInput
+                            name="password"
+                            label="Password"
+                            placeholder="Must be at least 8 characters"
+                            type={showPasswords ? "text" : "password"}
+                            svg={showPasswords ? <AiFillEyeInvisible /> : <AiFillEye />}
+                            svgClickHandler={() => setShowPasswords(prev => !prev)}
+                        />
+                        {formType === "register" && (
+                            <>
+                                <FormikTextInput
+                                    name="confirmPassword"
+                                    label="Confirm Password"
+                                    placeholder="Must be at least 8 characters"
+                                    type={showPasswords ? "text" : "password"}
+                                    svg={showPasswords ? <AiFillEyeInvisible /> : <AiFillEye />}
+                                    svgClickHandler={() => setShowPasswords(prev => !prev)}
+                                />
+                                <div className="meta-inputs">
+                                    <FormikDateInput
+                                        name="dateOfBirth"
+                                        label="Date Of Birth (optional)"
+                                        style={inputsStacked ? { width: "100%" } : { width: "60%" }}
+                                        minDate={minBirthDate}
+                                        maxDate={maxBirthDate}
+                                    />
+                                    <FormikSelectionInput
+                                        values={genders}
+                                        label="Gender"
+                                        style={inputsStacked ? { width: "100%" } : { width: "35%" }}
+                                        name="gender"
+                                        optional={true}
+                                    />
+                                </div>
+                            </>
+                        )}
+                        <Button bold type="submit">{formType === "register" ? "Register" : "Log In"}</Button>
+                    </Form>
+                </Formik>
             </AuthForm>
             {isFullscreen && (
                 <div style={{ textAlign: "center" }}>
