adee8c711ec739fdd8234d829739b6186626d5e7
Florian Kulig <florian.kulig@web.de>
2021-01-28 00:07:13 +0100

    removed bad import

--- src/helpers/getCurrentTabFromRoute.ts
+++ src/helpers/getCurrentTabFromRoute.ts
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 import { devTabNames, testerTabNames } from 'ts/constants';
 
 const getCurrentTabFromRoute = (): string => {
