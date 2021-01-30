3656c1b86ad03a18bf0aa6a0327aecbaf495f528
Florian Kulig <florian.kulig@web.de>
2021-01-30 22:25:59 +0100

    removed imports/exports

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,3 @@
-export { default as evaluateEmailRes } from './evaluateEmailRes';
 export { default as getTextColor } from './getTextColor';
 export { default as capitalized } from './capitalized';
 export { default as getCurrentTabFromRoute } from './getCurrentTabFromRoute';
