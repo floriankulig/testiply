b232fafba40b90250da2e25221ca153593f3640f
Florian Kulig <florian.kulig@web.de>
2021-01-30 22:25:48 +0100

    removed email res code evaluation

--- src/helpers/evaluateEmailRes.ts
+++
@@ -1,7 +0,0 @@
-const evaluateEmailRes = (resIdx: number): string => {
-  if (resIdx === 2) return "This E-Mail Address is already signed up!";
-  else if (resIdx === 3) return "Please put in a valid E-Mail Address.";
-  else return "Oops... something went wrong. Please retry.";
-};
-
-export default evaluateEmailRes;
