0cfabc62432d1794c90ba3302dbab7b2e1e32637
Florian Kulig <florian.kulig@web.de>
2021-02-04 20:38:55 +0100

    added App type

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -37,4 +37,25 @@ export interface Category {
 export type InputType = "email" | "password";
 export type FormType = "login" | "register" | "dev_register";
 
+export interface App {
+  name: string;
+  desc: string;
+  websiteUrl: string; //Website oder Github oder FaceBook
+  platform: Platform;
+  icon: string;
+  screenshots: string[];
+  rating: number;
+  downloads: number;
+  developer_id: number; // dev/dev_id
+  developer_name: string;
+  _id: string;
+}
+
+interface TodayAppWidgets {
+  heading: string;
+  opensLongDescription: boolean;
+  _id: string;
+  apps: App[];
+}
+
 export type UserType = "tester" | "dev";
