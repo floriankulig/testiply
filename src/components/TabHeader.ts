58a659c3b8d231dea9cb60434c1c91b73f038deb
Florian Kulig <florian.kulig@web.de>
2021-02-04 20:49:53 +0100

    added styled components for tab headers

--- src/components/TabHeader.ts
+++ src/components/TabHeader.ts
@@ -1,6 +1,52 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const TabHeader = styled.h1`
-  margin: 0;
+  margin: 0 0 1.5em;
+  display: flex;
+  align-items: center;
+  span {
+    margin: 0;
+    display: inline-flex;
+    align-items: center;
+  }
+
+  i {
+    color: grey;
+    font-weight: normal;
+    font-size: 2.1rem;
+    margin: 0 7px;
+    transform: scaleX(0.7);
+  }
+
+  @media (${({ theme }) => theme.bp.small}) {
+    flex-direction: row;
+  }
+`;
+
+export const BasePath = styled.span<{ hasIcon?: boolean }>`
+  position: relative;
   color: var(--navy);
+  padding-left: ${(p) => p.hasIcon && "30px"};
+  transition: 0.25s var(--easing);
+  transition-property: color, padding;
+  z-index: 1;
+  svg {
+    position: absolute;
+    left: -4px;
+  }
+
+  ${(p) =>
+    p.hasIcon &&
+    css`
+      &:hover {
+        cursor: pointer;
+        color: var(--primary);
+      }
+    `}
+`;
+
+export const CustomPath = styled.span`
+  color: grey;
+  font-weight: normal;
+  font-size: 1.7rem;
 `;
