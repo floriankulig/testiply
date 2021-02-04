172d2ece7c3441faa9ad2748a484ab9fb1c90f10
Florian Kulig <florian.kulig@web.de>
2021-02-04 17:49:34 +0100

    refactored code for context usage

--- src/components/layouts/sidebar/Sidebar.tsx
+++ src/components/layouts/sidebar/Sidebar.tsx
@@ -1,85 +1,91 @@
-import { useOnClickOutside } from "hooks"
+import { useOnClickOutside } from "hooks";
 import { useRef } from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import { TabRow, Tabs } from "./Tabs";
-import { BiLogOut } from "react-icons/bi"
-import { useAuthValue, useSelectedPlatformValue } from "context";
+import { BiLogOut } from "react-icons/bi";
+import { useAuthValue, useFiltersValue } from "context";
 import { SelectionInput } from "components/SelectionInput";
 import { platforms } from "ts";
 import { useRouter } from "next/router";
 
 interface StyledSidebarProps {
-    open: boolean;
+  open: boolean;
 }
 
 const StyledSidebar = styled.div<StyledSidebarProps>`
-    background-color: var(--layout-nav-background);
-    width: var(--sidebar-width);
-    transform: ${p => p.open ? "translateX(0)" : "translateX(-100%)"};
-    @media (${({ theme }) => theme.bp.big}){
-        transform: translateX(0);
-    } 
-    height: 100vh;
-    position: fixed;
-    z-index: 99999;
-    transition: var(--sidebarDuration) transform var(--easing);
-    overflow-y: auto;
+  background-color: var(--layout-nav-background);
+  width: var(--sidebar-width);
+  transform: ${(p) => (p.open ? "translateX(0)" : "translateX(-100%)")};
+  @media (${({ theme }) => theme.bp.big}) {
+    transform: translateX(0);
+  }
+  height: 100vh;
+  position: fixed;
+  z-index: 99999;
+  transition: var(--sidebarDuration) transform var(--easing);
+  overflow-y: auto;
 
-    .logo{
-        display: inline-flex;
-        align-items: center; 
-        font-weight: bold;
-        font-size: 1.5rem;
-        width: max-content; 
-        margin: 0 50px;
-        height: var(--header-height);
-    }
-`
+  .logo {
+    display: inline-flex;
+    align-items: center;
+    font-weight: bold;
+    font-size: 1.5rem;
+    width: max-content;
+    margin: 0 50px;
+    height: var(--header-height);
+  }
+`;
 
 const SidebarContent = styled.div`
-    min-height: calc(100vh - var(--header-height));
-    padding: 3em 15px 5em;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  min-height: calc(100vh - var(--header-height));
+  padding: 3em 15px 5em;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 interface SidebarProps {
-    open: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const Sidebar: React.FC<SidebarProps> = ({ open, setOpen }) => {
-    const { selectedPlatform, setSelectedPlatform } = useSelectedPlatformValue()
-    const { pathname } = useRouter()
-    const isDevRoute = pathname.split("/")[1] === "dev"
+  const { selectedPlatform, setSelectedPlatform } = useFiltersValue();
+  const { pathname } = useRouter();
+  const isDevRoute = pathname.split("/")[1] === "dev";
 
-    const ref = useRef<HTMLDivElement>();
-    const { currentUser } = useAuthValue();
-    useOnClickOutside(ref, () => setOpen(false))
+  const ref = useRef<HTMLDivElement>();
+  const { currentUser } = useAuthValue();
+  useOnClickOutside(ref, () => setOpen(false));
 
-    return (
-        <StyledSidebar ref={ref} open={open}>
-            <div className="logo">BetaStore </div>
-            <SidebarContent>
-                <div>
-                    <Tabs
-                        tabTypes={isDevRoute ? "dev" : "tester"}
-                        setSidebarOpen={setOpen}
-                    />
-                    {!isDevRoute && (
-                        <SelectionInput style={{ marginBottom: "6em" }} selection={selectedPlatform} setSelection={setSelectedPlatform} label="Platform" values={platforms} />
-                    )}
-                </div>
-                {currentUser && (
-                    <TabRow selected={false} icon style={{ marginTop: "3em" }}>
-                        <span>
-                            <BiLogOut />
-                        </span>
-                        Logout
-                    </TabRow>
-                )}
-            </SidebarContent>
-        </StyledSidebar>
-    )
-}
+  return (
+    <StyledSidebar ref={ref} open={open}>
+      <div className="logo">BetaStore </div>
+      <SidebarContent>
+        <div>
+          <Tabs
+            tabTypes={isDevRoute ? "dev" : "tester"}
+            setSidebarOpen={setOpen}
+          />
+          {!isDevRoute && (
+            <SelectionInput
+              style={{ marginBottom: "6em" }}
+              selection={selectedPlatform}
+              setSelection={setSelectedPlatform}
+              label="Platform"
+              values={platforms}
+            />
+          )}
+        </div>
+        {currentUser && (
+          <TabRow selected={false} icon style={{ marginTop: "3em" }}>
+            <span>
+              <BiLogOut />
+            </span>
+            Logout
+          </TabRow>
+        )}
+      </SidebarContent>
+    </StyledSidebar>
+  );
+};
