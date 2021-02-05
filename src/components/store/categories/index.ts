9b136d8479a0eb7344b4b8ae98d0f59d6a94465d
Florian Kulig <florian.kulig@web.de>
2021-02-05 17:21:49 +0100

    added better responsitivity for grid

--- src/components/store/categories/index.ts
+++ src/components/store/categories/index.ts
@@ -2,21 +2,32 @@ import styled from "styled-components";
 
 const CategoryGrid = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: 1fr 1fr;
+  @media (${({ theme }) => theme.bp.big}) {
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
   gap: 2.5em;
 `;
 
 const CategoryChip = styled.li`
-  padding: 1.2em 1.5em;
+  padding: 0.7em 1.3em;
+  display: flex;
+  align-items: center;
   border-radius: var(--border-radius);
   box-shadow: var(--btn-shadow);
   cursor: pointer;
   opacity: 0;
+  font-size: clamp(1rem, 2vw, 1.2rem);
   animation: fadeUp 0.3s var(--easing) forwards;
   transition: box-shadow 300ms var(--easing);
 
   &:hover {
     box-shadow: var(--btn-shadow-hover);
+  }
+
+  svg {
+    margin-right: 0.5em;
+    font-size: clamp(1.5rem, 4vw, 2rem);
   }
 `;
 
