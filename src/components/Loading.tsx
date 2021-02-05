234a1a6f4e14947f180850cbcd761274ab81bcb4
Florian Kulig <florian.kulig@web.de>
2021-02-05 16:07:18 +0100

    Readability: Adjusted Formatting

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,83 +1,97 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 interface Props {
-    color: string
+  color: string;
 }
 
 const EllipsisLoader = styled.div<Props>`
-    display: inline-block;
-    position: relative;
+  display: inline-block;
+  position: relative;
 
-    div {
-        position: absolute;
-        top: 41.25%;
-        width: 16.25%;
-        height: 48.75%;
-        border-radius: 50%;
-        background: #fff;
-        animation-timing-function: cubic-bezier(0, 1, 1, 0);
-        background: ${p => p.color === "primary" ? p.theme.primary : !p.color ? " #c8c8c8" : p.color}
-    }
+  div {
+    position: absolute;
+    top: 41.25%;
+    width: 16.25%;
+    height: 48.75%;
+    border-radius: 50%;
+    background: #fff;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+    background: ${(p) =>
+      p.color === "primary"
+        ? p.theme.primary
+        : !p.color
+        ? " #c8c8c8"
+        : p.color};
+  }
 
-    div:nth-child(1) {
-        left: 10%;
-        animation: lds-ellipsis1 0.6s infinite;
+  div:nth-child(1) {
+    left: 10%;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+  div:nth-child(2) {
+    left: 10%;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  div:nth-child(3) {
+    left: 40%;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  div:nth-child(4) {
+    left: 70%;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
     }
-    div:nth-child(2) {
-        left: 10%;
-        animation: lds-ellipsis2 0.6s infinite;
+    100% {
+      transform: scale(1);
     }
-    div:nth-child(3) {
-        left: 40%;
-        animation: lds-ellipsis2 0.6s infinite;
+  }
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
     }
-    div:nth-child(4) {
-        left: 70%;
-        animation: lds-ellipsis3 0.6s infinite;
+    100% {
+      transform: scale(0);
     }
-    @keyframes lds-ellipsis1 {
-        0% {
-            transform: scale(0);
-        }
-        100% {
-            transform: scale(1);
-        }
+  }
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
     }
-    @keyframes lds-ellipsis3 {
-        0% {
-            transform: scale(1);
-        }
-        100% {
-            transform: scale(0);
-        }
+    100% {
+      transform: translate(184.61%, 0);
     }
-    @keyframes lds-ellipsis2 {
-        0% {
-            transform: translate(0, 0);
-        }
-        100% {
-            transform: translate(184.61%, 0);
-        }
-    }
-
-`
+  }
+`;
 
 interface LoaderProps {
-    color?: string;
-    size?: number;
-    className?: string;
-    style?: Object
+  color?: string;
+  size?: number;
+  className?: string;
+  style?: Object;
 }
 
-export const Loading: React.FC<LoaderProps> = ({ color = '#c8c8c8', size = 60, className, style = {} }) => {
-    const circles = [...Array(4)].map((_, index) => <div key={index} />)
+export const Loading: React.FC<LoaderProps> = ({
+  color = "#c8c8c8",
+  size = 60,
+  className,
+  style = {},
+}) => {
+  const circles = [...Array(4)].map((_, index) => <div key={index} />);
 
-    return (
-        <EllipsisLoader className={className} style={{
-            ...style,
-            width: size,
-            height: size / 3
-        }}
-            color={color}>{circles}</EllipsisLoader>
-    )
-}
+  return (
+    <EllipsisLoader
+      className={className}
+      style={{
+        ...style,
+        width: size,
+        height: size / 3,
+      }}
+      color={color}
+    >
+      {circles}
+    </EllipsisLoader>
+  );
+};
