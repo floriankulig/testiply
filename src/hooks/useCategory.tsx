5ab4175f4ebc3a0b0c3bc43e3e0c993f12bf94f8
Florian Kulig <florian.kulig@web.de>
2021-02-05 09:47:47 +0100

    added custom useCategory hook

--- src/hooks/useCategory.tsx
+++ src/hooks/useCategory.tsx
@@ -1,19 +1,56 @@
+import axios from "axios";
+import { useFiltersValue } from "context";
 import { useState, useEffect } from "react";
 import { App, categories, Category, CategoryID } from "ts";
+import { api_url } from "ts/constants";
 
-type DataType = Category | null;
+type CategoryType = Category | null;
 interface ReturnType {
-  selectedCategory: DataType;
-  setSelectedCategory: React.Dispatch<React.SetStateAction<DataType>>;
+  selectedCategory: CategoryType;
+  setSelectedCategory: React.Dispatch<React.SetStateAction<CategoryType>>;
+  apps: Array<App | null>;
 }
 
-export const useCategory = (initial?: DataType): ReturnType => {
-  const [selectedCategory, setSelectedCategory] = useState<DataType>(initial);
-  const [data, setData] = useState<Array<App | null>>([]);
+export const useCategory = (initial?: CategoryType): ReturnType => {
+  const { selectedPlatform, searchQuery } = useFiltersValue();
+  const [selectedCategory, setSelectedCategory] = useState<CategoryType>(
+    initial
+  );
+  const [apps, setApps] = useState<Array<App>>([]);
 
   useEffect(() => {
-    console.log(selectedCategory);
-  }, [selectedCategory]);
+    if (selectedCategory === null) {
+      setApps([]);
+      return;
+    }
+    let newApps: App[];
 
-  return { selectedCategory, setSelectedCategory };
+    let query: string = `?category=${selectedCategory.id}`;
+    query =
+      selectedPlatform === "all"
+        ? query + ""
+        : query + `&platform[]=${selectedPlatform}`;
+
+    axios
+      .get(`${api_url}/categoryApps${query}`)
+      .then((res) => (newApps = res.data))
+      .catch((err) => console.log(err));
+
+    if (JSON.stringify(newApps) !== JSON.stringify(apps)) {
+      setApps(newApps);
+    }
+  }, [selectedCategory, selectedPlatform]);
+
+  useEffect(() => {
+    if (!apps || !selectedCategory) return;
+
+    const filteredApps = apps?.filter((app) => app.name.includes(searchQuery));
+    if (JSON.stringify(filteredApps) !== JSON.stringify(apps)) {
+      setApps(filteredApps);
+    }
+    console.log(searchQuery);
+    console.log(filteredApps);
+  }, [searchQuery, apps]);
+
+  return { selectedCategory, setSelectedCategory, apps };
 };
