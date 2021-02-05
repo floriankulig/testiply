b505a8a05a68518ba19780be3ed473f4e7033ce4
Florian Kulig <florian.kulig@web.de>
2021-02-05 16:26:46 +0100

    hooked selected tab with useEffect

--- src/context/selected-tab-context.tsx
+++ src/context/selected-tab-context.tsx
@@ -1,27 +1,29 @@
 import { getCurrentTabFromRoute } from "helpers";
+import { useRouter } from "next/router";
 import { createContext, useContext, useState, useEffect } from "react";
 
 export type ContextType = {
-    selectedTab?: string;
-    setSelectedTab?: React.Dispatch<React.SetStateAction<string>>;
-}
+  selectedTab?: string;
+  setSelectedTab?: React.Dispatch<React.SetStateAction<string>>;
+};
 
 export const SelectedTabContext = createContext<Partial<ContextType>>({});
 
 export const SelectedTabProvider = ({ children }) => {
-    const [selectedTab, setSelectedTab] = useState<string>(getCurrentTabFromRoute());
+  const [selectedTab, setSelectedTab] = useState<string>(
+    getCurrentTabFromRoute()
+  );
+  const { pathname } = useRouter();
 
-    useEffect(() => {
-        console.log(selectedTab)
-    }, [selectedTab]);
+  useEffect(() => {
+    setSelectedTab(getCurrentTabFromRoute);
+  }, [pathname]);
 
-    return (
-        <SelectedTabContext.Provider
-            value={{ selectedTab, setSelectedTab }}
-        >
-            {children}
-        </SelectedTabContext.Provider>
-    );
+  return (
+    <SelectedTabContext.Provider value={{ selectedTab, setSelectedTab }}>
+      {children}
+    </SelectedTabContext.Provider>
+  );
 };
 
 export const useSelectedTabValue = () => useContext(SelectedTabContext);
