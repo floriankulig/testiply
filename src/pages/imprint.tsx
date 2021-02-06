67e1284f918043ad83640baea953aa1bc2256a2c
Florian Kulig <florian.kulig@web.de>
2021-02-06 17:12:04 +0100

    calling useRouter at top level

--- src/pages/imprint.tsx
+++ src/pages/imprint.tsx
@@ -1,79 +1,94 @@
 import { Button } from "components/Button";
 import { InfoFooter } from "components/InfoFooter";
 import { InfoPageHeader } from "components/InfoPageHeader";
-import { capitalized, getPathname, getTextColor } from "helpers";
+import { capitalized, getTextColor } from "helpers";
 import { useStaticJSON } from "hooks";
 import { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import styled from "styled-components";
 import { theme } from "styles";
 
 const Content = styled.div`
-    margin-top: 1em;
-    min-height: 105vh;
-    span {
-        font-size: 1.5rem;
-        color: #a5a5a5;
-    }
+  margin-top: 1em;
+  min-height: 105vh;
+  span {
+    font-size: 1.5rem;
+    color: #a5a5a5;
+  }
 
-    ${Button}{
-        margin-top: 1em;
-    }
+  ${Button} {
+    margin-top: 1em;
+  }
 `;
 const ContentBlock = styled.div`
-    margin-bottom: 3em;
-    &:first-of-type {
-        margin-top: 2em;
-    }
-    h2{
-        color: ${({ theme }) => theme.primary};
-        margin-bottom: 0.5em;
-    }
-    p {
-        margin: 0;
-    }
+  margin-bottom: 3em;
+  &:first-of-type {
+    margin-top: 2em;
+  }
+  h2 {
+    color: ${({ theme }) => theme.primary};
+    margin-bottom: 0.5em;
+  }
+  p {
+    margin: 0;
+  }
 `;
 
 interface Paragraph {
-    title: string;
-    text: string;
+  title: string;
+  text: string;
 }
 
 const Imprint: NextPage = () => {
-    const [lang, setLang] = useState<"en" | "de">("en")
-    const paragraphs = useStaticJSON(lang, "imprint")
+  const { pathname } = useRouter();
+  const [lang, setLang] = useState<"en" | "de">("en");
+  const paragraphs = useStaticJSON(lang, "imprint");
 
-    const toggleLang = () => setLang(lang === "de" ? "en" : "de")
-    const buttonText = lang === "de" ? "Zu Englisch wechseln" : "Switch to German"
-    const pathname = getPathname()
+  const toggleLang = () => setLang(lang === "de" ? "en" : "de");
+  const buttonText =
+    lang === "de" ? "Zu Englisch wechseln" : "Switch to German";
 
-    return (
-        <>
-            <Head>
-                <title>Imprint | Beta App Store</title>
-            </Head>
-            <InfoPageHeader className="container" style={{
-                color: getTextColor(theme.layoutContentBg)
-            }} />
-            <Content className="container">
-                {pathname.map(link => (
-                    <span>/ {capitalized(link)}</span>
-                ))}
-                {paragraphs && paragraphs.map((paragraph: Paragraph) => (
-                    <ContentBlock>
-                        <h2>{paragraph.title}</h2>
-                        {paragraph.text.split("\n").map(text => (
-                            <p>{text}</p>
-                        ))}
-                    </ContentBlock>
-                ))}
-                <Button big onClick={toggleLang} onKeyDown={toggleLang} aria-label="Toggle language">{buttonText}</Button>
-            </Content>
-            <InfoFooter />
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Imprint | Beta App Store</title>
+      </Head>
+      <InfoPageHeader
+        className="container"
+        style={{
+          color: getTextColor(theme.layoutContentBg),
+        }}
+      />
+      <Content className="container">
+        {pathname
+          .split("/")
+          .slice(1)
+          .map((link) => (
+            <span>/ {capitalized(link)}</span>
+          ))}
+        {paragraphs &&
+          paragraphs.map((paragraph: Paragraph) => (
+            <ContentBlock key={paragraph.title}>
+              <h2>{paragraph.title}</h2>
+              {paragraph.text.split("\n").map((text) => (
+                <p>{text}</p>
+              ))}
+            </ContentBlock>
+          ))}
+        <Button
+          big
+          onClick={toggleLang}
+          onKeyDown={toggleLang}
+          aria-label="Toggle language"
+        >
+          {buttonText}
+        </Button>
+      </Content>
+      <InfoFooter />
+    </>
+  );
+};
 
-
-export default Imprint
+export default Imprint;
