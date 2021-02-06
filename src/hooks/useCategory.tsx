2000c548dac1a2d86a7789dffd7870b8852777b6
Florian Kulig <florian.kulig@web.de>
2021-02-06 17:12:45 +0100

    added Support for url sharing / -hook level filter

--- src/hooks/useCategory.tsx
+++ src/hooks/useCategory.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useFiltersValue } from "context";
+import { getCurrentCategoryFromRoute } from "helpers";
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
-import { App, categories, Category, CategoryID } from "ts";
+import { App, Category } from "ts";
 import { api_url } from "ts/constants";
 
 type CategoryType = Category | null;
@@ -11,20 +13,26 @@ interface ReturnType {
   apps: App[];
 }
 
-export const useCategory = (initial?: CategoryType): ReturnType => {
-  const { selectedPlatform, searchQuery } = useFiltersValue();
-  const [selectedCategory, setSelectedCategory] = useState<CategoryType>(
-    initial
-  );
-  const [apps, setApps] = useState<App[]>([]);
+export const useCategory = (): ReturnType => {
+  //Helper Hooks
+  const { query } = useRouter();
+  const { selectedPlatform } = useFiltersValue();
 
+  // Getting current category from url => support for url sharing
+  const [selectedCategory, setSelectedCategory] = useState<CategoryType>(
+    getCurrentCategoryFromRoute(query)
+  );
+  useEffect(() => {
+    setSelectedCategory(getCurrentCategoryFromRoute(query));
+  }, [query]);
+
+  // Requesting Apps Logic
+  const [apps, setApps] = useState<App[]>([]);
   useEffect(() => {
     if (selectedCategory === null) {
       setApps([]);
       return;
     }
-    let newApps: App[];
-
     let query: string = `?category=${selectedCategory.id}`;
     query =
       selectedPlatform === "all"
@@ -33,24 +41,13 @@ export const useCategory = (initial?: CategoryType): ReturnType => {
 
     axios
       .get(`${api_url}/categoryApps${query}`)
-      .then((res) => (newApps = res.data))
+      .then((res) => {
+        if (JSON.stringify(res.data) !== JSON.stringify(apps)) {
+          setApps(res.data);
+        }
+      })
       .catch((err) => console.log(err));
-
-    if (JSON.stringify(newApps) !== JSON.stringify(apps)) {
-      setApps(newApps);
-    }
   }, [selectedCategory, selectedPlatform]);
-
-  useEffect(() => {
-    if (!apps || !selectedCategory) return;
-
-    const filteredApps = apps?.filter((app) => app.name.includes(searchQuery));
-    if (JSON.stringify(filteredApps) !== JSON.stringify(apps)) {
-      setApps(filteredApps);
-    }
-    console.log(searchQuery);
-    console.log(filteredApps);
-  }, [searchQuery, apps]);
 
   return { selectedCategory, setSelectedCategory, apps };
 };
