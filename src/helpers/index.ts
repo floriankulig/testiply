14633e0647261f016737907cb808f4d18e5afc98
Florian Kulig <florian.kulig@web.de>
2021-02-06 17:07:58 +0100

    adjusted imports

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,4 @@
 export { default as getTextColor } from "./getTextColor";
 export { default as capitalized } from "./capitalized";
 export { default as getCurrentTabFromRoute } from "./getCurrentTabFromRoute";
-export { default as getPathname } from "./getPathname";
+export { default as getCurrentCategoryFromRoute } from "./getCurrentCategoryFromRoute";
