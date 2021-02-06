22941c2c5a9a37909f6699e5dccbd73d5886c4ef
Florian Kulig <florian.kulig@web.de>
2021-02-06 18:01:38 +0100

    sharing app grid component in layout folder

---
+++ src/components/layouts/AppGrid.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const AppGrid = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  @media (${({ theme }) => theme.bp.big}) {
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
+  gap: 2.5em;
+
+  h2.loading {
+    display: inline-flex;
+    justify-content: center;
+    color: #c8c8c8;
+    margin-top: 10vh;
+  }
+`;
