33350ffcd64cc70457d4b9bf2ed5371355c612b9
Florian Kulig <florian.kulig@web.de>
2021-02-06 17:07:21 +0100

    removed invalid calling hook

--- src/helpers/getPathname.ts
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-
-const getPathname = (): string[] => {
-  const router = useRouter();
-
-  return router.pathname.split('/').slice(1);
-};
-export default getPathname;
