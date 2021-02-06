927e390838c5d2145940060148ad71387b594b31
Florian Kulig <florian.kulig@web.de>
2021-02-06 16:31:14 +0100

    removed date formatting function

--- src/helpers/getFormattedDate.ts
+++
@@ -1,9 +0,0 @@
-const getFormattedDate = (date: Date): string => {
-  let year = date.getFullYear();
-  let month = (1 + date.getMonth()).toString().padStart(2, '0');
-  let day = date.getDate().toString().padStart(2, '0');
-
-  return month + '/' + day + '/' + year;
-};
-
-export default getFormattedDate;
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,4 +2,3 @@ export { default as getTextColor } from "./getTextColor";
 export { default as capitalized } from "./capitalized";
 export { default as getCurrentTabFromRoute } from "./getCurrentTabFromRoute";
 export { default as getPathname } from "./getPathname";
-export { default as getFormattedDate } from "./getFormattedDate";
