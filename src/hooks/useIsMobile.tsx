b94e085483346d04631e8a0694c15312e66c1180
Florian Kulig <florian.kulig@web.de>
2021-02-07 16:06:51 +0100

    setting default to true (mobile first)

--- src/hooks/useIsMobile.tsx
+++ src/hooks/useIsMobile.tsx
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
 
 export const useIsMobile = (breakpoint: number = 1080): boolean => {
-    const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(true);
 
-    const onResize = (e) => {
-        if (e.currentTarget.innerWidth > breakpoint) {
-            setIsMobile(false)
-        } else {
-            setIsMobile(true)
-        }
+  const onResize = (e) => {
+    if (e.currentTarget.innerWidth > breakpoint) {
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
     }
+  };
 
-    useEffect(() => {
-        // set this as an initial value without having to resize first
-        setIsMobile(window.innerWidth <= breakpoint);
+  useEffect(() => {
+    // set this as an initial value without having to resize first
+    setIsMobile(window.innerWidth <= breakpoint);
 
-        // handle on resize
-        window.addEventListener("resize", onResize);
-        return () => window.removeEventListener("resize", onResize)
-    }, [])
+    // handle on resize
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
 
-    return isMobile
-}
+  return isMobile;
+};
