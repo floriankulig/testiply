f5822c17b5b3a12a91f27c77b892397bf8fcc5b0
Florian Kulig <florian.kulig@web.de>
2021-02-08 13:23:52 +0100

    reworked features section (svgs, margin, format)

--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -1,100 +1,119 @@
 import { lighten } from "polished";
 import { useEffect, useRef } from "react";
-import ScrollOut from "scroll-out"
-import { RiCodeSSlashFill, RiFeedbackLine, RiShoppingBag3Line } from "react-icons/ri";
-import styled from "styled-components"
+import ScrollOut from "scroll-out";
+import {
+  RiCodeSSlashFill,
+  RiFeedbackLine,
+  RiShoppingBag3Line,
+} from "react-icons/ri";
+import styled from "styled-components";
 
 const FeaturesSection = styled.div`
-    margin-top: 400px;
-    position: relative;
-    background: var(--layout-content-background);
-    padding: 180px 0 200px;
+  margin-top: 500px;
+  position: relative;
+  background: var(--layout-content-background);
+  padding: 180px 0 200px;
 
-    .triangle{
-        position: absolute;
-        width: 100%;
-        height: 0;
-        left: 0;
-        border-style: solid;
+  .triangle {
+    position: absolute;
+    width: 100%;
+    height: 0;
+    left: 0;
+    border-style: solid;
 
-        &-1{
-            top: 0;
-            border-width: 80px 100vw 0 0;
-            border-color: #ffffff transparent transparent transparent;
-        }
-        &-2{
-            bottom: 0;
-            border-width: 0 0 80px 100vw;
-            border-color: transparent transparent #ffffff transparent;
-        }
+    &-1 {
+      top: 0;
+      border-width: 80px 100vw 0 0;
+      border-color: #ffffff transparent transparent transparent;
     }
+    &-2 {
+      bottom: 0;
+      border-width: 0 0 80px 100vw;
+      border-color: transparent transparent #ffffff transparent;
+    }
+  }
 `;
 
 const FeaturesContainer = styled.ul`
-    display: flex;
-    justify-content: space-between;
-    flex-direction: column;
-    @media (${({ theme }) => theme.bp.big}) {
-        flex-direction: row;
-    }
-`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  @media (${({ theme }) => theme.bp.big}) {
+    flex-direction: row;
+  }
+`;
 
 const Feature = styled.li`
-    width: 370px;
-    max-width: 100%;
-    margin: 0 auto;
-    margin-bottom: 4em;
-    color: var(--navy);
-    
+  width: 370px;
+  max-width: 100%;
+  margin: 0 auto;
+  margin-bottom: 4em;
+  color: var(--navy);
 
-    svg{
-        width: 20%;
-        height: 20%;
-        color: var(--primary);
-        margin-left: -4%;
-    }
+  svg {
+    width: 40px;
+    height: 40px;
+    color: var(--primary);
+  }
 
-    p {
-        color: ${({ theme }) => lighten(0.2, theme.navy)};
-    }
+  p {
+    color: ${({ theme }) => lighten(0.2, theme.navy)};
+  }
 
-    @media (${({ theme }) => theme.bp.big}) {
-        margin: 0;
-        width: clamp(230px, 22vw, 330px);
-        &:nth-child(1){ transition-delay: 0.1s; }
-        &:nth-child(2){ transition-delay: 0.2s; }
-        &:nth-child(3){ transition-delay: 0.3s; }
+  @media (${({ theme }) => theme.bp.big}) {
+    margin: 0;
+    width: clamp(230px, 22vw, 330px);
+    &:nth-child(1) {
+      transition-delay: 0.1s;
     }
-`
+    &:nth-child(2) {
+      transition-delay: 0.2s;
+    }
+    &:nth-child(3) {
+      transition-delay: 0.3s;
+    }
+  }
+`;
 
 export const Features: React.FC = () => {
-    const features = useRef([])
+  const features = useRef([]);
 
-    useEffect(() => {
-        ScrollOut({ targets: features.current, once: true, threshold: 0.5 })
-    }, []);
+  useEffect(() => {
+    ScrollOut({ targets: features.current, once: true, threshold: 0.5 });
+  }, []);
 
-    return (
-        <FeaturesSection>
-            <div className="triangle triangle-1" />
-            <FeaturesContainer className="container">
-                <Feature ref={el => (features.current[0] = el)}>
-                    <RiShoppingBag3Line />
-                    <h2>Large App Offer</h2>
-                    <p>Our App-Market is always expanding and offering applications from all sorts of categories. Without having to download the source code, you can test any app you like.</p>
-                </Feature>
-                <Feature ref={el => (features.current[1] = el)}>
-                    <RiFeedbackLine />
-                    <h2>Easy Feedback</h2>
-                    <p>We provide easy ways in our User Interface to give fast and easy feedback to featured developers, because we value your time. </p>
-                </Feature>
-                <Feature ref={el => (features.current[2] = el)}>
-                    <RiCodeSSlashFill />
-                    <h2>Great For Developers</h2>
-                    <p>Having a platform to offer apps for Beta-Testing, developers can get valueable and fast feedback from users who know what could be important for your app.</p>
-                </Feature>
-            </FeaturesContainer>
-            <div className="triangle triangle-2" />
-        </FeaturesSection>
-    )
-}
+  return (
+    <FeaturesSection>
+      <div className="triangle triangle-1" />
+      <FeaturesContainer className="container">
+        <Feature ref={(el) => (features.current[0] = el)}>
+          <RiShoppingBag3Line />
+          <h2>Large App Offer</h2>
+          <p>
+            Our App-Market is always expanding and offering applications from
+            all sorts of categories. Without having to download the source code,
+            you can test any app you like.
+          </p>
+        </Feature>
+        <Feature ref={(el) => (features.current[1] = el)}>
+          <RiFeedbackLine />
+          <h2>Easy Feedback</h2>
+          <p>
+            We provide easy ways in our User Interface to give fast and easy
+            feedback to featured developers, because we value your time.{" "}
+          </p>
+        </Feature>
+        <Feature ref={(el) => (features.current[2] = el)}>
+          <RiCodeSSlashFill />
+          <h2>Great For Developers</h2>
+          <p>
+            Having a platform to offer apps for Beta-Testing, developers can get
+            valueable and fast feedback from users who know what could be
+            important for your app.
+          </p>
+        </Feature>
+      </FeaturesContainer>
+      <div className="triangle triangle-2" />
+    </FeaturesSection>
+  );
+};
