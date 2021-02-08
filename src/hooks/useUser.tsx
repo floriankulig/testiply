6394076e9f77ad469cf23f50e273934cab4ca521
Florian Kulig <florian.kulig@web.de>
2021-02-08 21:38:50 +0100

    using removeCookie function

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -5,29 +5,25 @@ import { useCookies } from "react-cookie";
 
 export const useUser = (): AuthContextType => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
-  const [cookie, setCookie] = useCookies(["token"]);
+  const [cookie, setCookie, removeCookie] = useCookies(["token"]);
 
   // Renew Cookie on App Mount and set Session User
   useEffect(() => {
     const currentToken = cookie["token"];
 
+    removeCookie("token");
+
     let newExpireDate = new Date();
     newExpireDate.setDate(newExpireDate.getDate() + 30);
     setCookie("token", currentToken, {
       sameSite: true,
-      httpOnly: true,
       expires: newExpireDate,
     });
-
-    setCurrentUser(currentToken);
   }, []);
 
   // Set Cookie to date in past / non-existant date
   const logout = (): void => {
-    setCookie("token", cookie, {
-      sameSite: true,
-      expires: new Date(0),
-    });
+    removeCookie("token");
     setCurrentUser(null);
   };
 
