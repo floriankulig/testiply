dfea21e7721180bb5368731daaaa850e3055d127
Florian Kulig <florian.kulig@web.de>
2021-02-11 17:57:26 +0100

    added margin for full-width grid components

--- src/components/layouts/AppGrid.ts
+++ src/components/layouts/AppGrid.ts
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const AppGrid = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(210px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
   @media (${({ theme }) => theme.bp.small}) {
     grid-template-columns: repeat(2, 1fr);
   }
   @media (${({ theme }) => theme.bp.big}) {
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
   }
   gap: clamp(1em, 3vw, 2.5em);
 
@@ -16,5 +16,9 @@ export const AppGrid = styled.ul`
     justify-content: center;
     color: #c8c8c8;
     margin-top: 10vh;
+  }
+
+  .full-grid-width {
+    margin-top: 4em;
   }
 `;
