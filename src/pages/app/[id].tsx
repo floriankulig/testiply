70b2dc44308b964adda639cdb1c1fd29d16c09d0
Florian Kulig <florian.kulig@web.de>
2021-02-13 23:48:36 +0100

    added page for app ids

---
+++ src/pages/app/[id].tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Today = () => {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+    </>
+  );
+};
+
+export default Today;
