97e53e38860f762d6634dcf56f4c4f478b1bb782
Florian Kulig <florian.kulig@web.de>
2021-02-15 15:39:46 +0100

    setting mobile default to false

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -1,75 +1,73 @@
-import { AuthInfoSidebar } from "components/auth"
+import { AuthInfoSidebar } from "components/auth";
 import { InfoFooter } from "components/InfoFooter";
 import { InfoPageHeader } from "components/InfoPageHeader";
 import { getTextColor } from "helpers";
 import { useIsMobile } from "hooks";
-import styled from "styled-components"
+import styled from "styled-components";
 import { theme } from "styles";
-import { FormType } from "ts"
+import { FormType } from "ts";
 
 const StyledAuthLayout = styled.div`
-    background-color: var(--layout-content-background);
-    display: flex;
-    flex-direction: column;
+  background-color: var(--layout-content-background);
+  display: flex;
+  flex-direction: column;
 
-    @media (${({ theme }) => theme.bp.big}){
-        flex-direction: row;
-        max-height: 100vh;
-    }
+  @media (${({ theme }) => theme.bp.big}) {
+    flex-direction: row;
+    max-height: 100vh;
+  }
 `;
 
 const ContentContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
 `;
-
 
 const FormContainer = styled.div`
-    width: 100%;
-    height: 100%;
-    display: grid;
-    place-items:center;
-    flex-direction: column;
-    min-height: calc(100vh - var(--header-height));
-    overflow-x: hidden;
-    @media (${({ theme }) => theme.bp.big}) {
-        height: 100vh;
-        max-height: 100vh;
-        width: calc(100vw - var(--auth-sidebar-width));
-    }
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-items: center;
+  flex-direction: column;
+  min-height: calc(100vh - var(--header-height));
+  overflow-x: hidden;
+  @media (${({ theme }) => theme.bp.big}) {
+    height: 100vh;
+    max-height: 100vh;
+    width: calc(100vw - var(--auth-sidebar-width));
+  }
 `;
 
-
-
 interface AuthLayoutProps {
-    children: React.ReactNode;
-    formType: FormType;
+  children: React.ReactNode;
+  formType: FormType;
 }
 
-export const AuthLayout: React.FC<AuthLayoutProps> = ({ children, formType }) => {
-    const isMobile = useIsMobile(1200);
+export const AuthLayout: React.FC<AuthLayoutProps> = ({
+  children,
+  formType,
+}) => {
+  const isMobile = useIsMobile(1200, false);
 
-    return (
-        <StyledAuthLayout style={{ minHeight: "100vh" }}>
-            {formType.includes("register") && !isMobile && (
-                <AuthInfoSidebar type={formType} />
-            )}
-            <ContentContainer>
-                {isMobile && <InfoPageHeader style={{
-                    color: getTextColor(theme.layoutContentBg),
-                    padding: `0 ${isMobile ? "5%" : "4em"}`
-                }} />}
-                <FormContainer>
-                    {children}
-                </FormContainer>
-            </ContentContainer>
-            {formType === "login" && !isMobile && (
-                <AuthInfoSidebar type={formType} />
-            )}
-            {isMobile && (
-                <InfoFooter />
-            )}
-        </StyledAuthLayout>
-    )
-}
+  return (
+    <StyledAuthLayout style={{ minHeight: "100vh" }}>
+      {formType.includes("register") && !isMobile && (
+        <AuthInfoSidebar type={formType} />
+      )}
+      <ContentContainer>
+        {isMobile && (
+          <InfoPageHeader
+            style={{
+              color: getTextColor(theme.layoutContentBg),
+              padding: `0 ${isMobile ? "5%" : "4em"}`,
+            }}
+          />
+        )}
+        <FormContainer>{children}</FormContainer>
+      </ContentContainer>
+      {formType === "login" && !isMobile && <AuthInfoSidebar type={formType} />}
+      {isMobile && <InfoFooter />}
+    </StyledAuthLayout>
+  );
+};
