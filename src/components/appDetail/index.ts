9d4dab6e72383da56ebfbc3ef6ac6417405cc95f
Florian Kulig <florian.kulig@web.de>
2021-02-16 17:30:17 +0100

    managing imports/exports for new styled components

--- src/components/appDetail/index.ts
+++ src/components/appDetail/index.ts
@@ -1,3 +1,23 @@
 import { Screenshot, ScreenshotSection } from "./Screenshots";
+import { HeroSection, IconWrapper, MetaInfo, StyledRow } from "./Hero";
+import {
+  RatingBar,
+  RatingBars,
+  RatingSection,
+  RatingSummary,
+  StarPercentageRating,
+} from "./Rating";
 
-export { Screenshot, ScreenshotSection };
+export {
+  Screenshot,
+  ScreenshotSection,
+  HeroSection,
+  IconWrapper,
+  MetaInfo,
+  StyledRow,
+  RatingBar,
+  RatingBars,
+  RatingSection,
+  RatingSummary,
+  StarPercentageRating,
+};
