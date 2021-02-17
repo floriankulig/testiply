c8aa96941b02bb8bd7810745d4eb8dcc75b45811
Florian Kulig <florian.kulig@web.de>
2021-02-17 11:08:39 +0100

    added styles for download button

--- src/components/appDetail/Hero.ts
+++ src/components/appDetail/Hero.ts
@@ -1,3 +1,4 @@
+import { StyledClickableDropdown } from "components/ClickableDropdown";
 import styled from "styled-components";
 const HeroSection = styled.section`
   margin-top: 3em;
@@ -6,15 +7,21 @@ const HeroSection = styled.section`
     margin-top: 4em;
     margin-bottom: 4em;
   }
+  ${StyledClickableDropdown} {
+    // after name and dev
+    opacity: 0;
+    animation: fadeUp 0.5s var(--easing) forwards 0.4s;
+  }
 `;
 const StyledRow = styled.div`
   max-height: 200px;
+  margin-top: 2em;
   display: flex;
 `;
 
 const IconWrapper = styled.div`
-  width: clamp(120px, 15vw, 200px);
-  height: clamp(120px, 15vw, 200px);
+  width: clamp(120px, 25vw, 200px);
+  height: clamp(120px, 25vw, 200px);
   min-width: 120px;
   min-height: 120px;
   border-radius: 20%;
@@ -25,6 +32,9 @@ const IconWrapper = styled.div`
 const MetaInfo = styled.div`
   padding: 0.5em 0;
   margin-left: 1.5em;
+  max-height: 200px;
+  display: flex;
+  flex-direction: column;
   h1 {
     font-size: clamp(30px, 5vw, 60px);
     margin: 0;
@@ -40,6 +50,9 @@ const MetaInfo = styled.div`
     flex-wrap: wrap;
     opacity: 0;
     animation: fadeUp 0.5s var(--easing) forwards 0.3s;
+  }
+  ${StyledClickableDropdown} {
+    margin-top: auto;
   }
 `;
 
