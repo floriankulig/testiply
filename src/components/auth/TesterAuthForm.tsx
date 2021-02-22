40c25c4940a5cb355f68141041ec5a7b630fd70f
Florian Kulig <florian.kulig@web.de>
2021-02-22 19:16:27 +0100

    moved overlay out of this component

--- src/components/auth/TesterAuthForm.tsx
+++ src/components/auth/TesterAuthForm.tsx
@@ -18,19 +18,7 @@ import { useRouter } from "next/router";
 import { capitalized } from "helpers";
 import { Loading } from "components/Loading";
 import { useCookies } from "react-cookie";
-
-const Overlay = styled.div`
-  display: grid;
-  place-items: center;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  width: 100vw;
-  height: 100vh;
-  z-index: 99;
-  transform: translate(-50%, -50%);
-  background: ${({ theme }) => rgba(theme.navy, 0.3)};
-`;
+import { Overlay } from "components/Overlay";
 
 const StyledModal = styled.div`
   width: 400px;
