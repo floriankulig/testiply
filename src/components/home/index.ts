3a7eb5908a22dd9043a9fe0bd768b24dc95b7d2d
Florian Kulig <florian.kulig@web.de>
2021-02-23 15:38:12 +0100

    removed newsletter component

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,9 +1,8 @@
 //Gather all Files needed for index.tsx page
-import { Hero } from './Hero';
-import { Features } from './Features';
-import { Benefits } from './Benefits';
-import { NewsLetter } from './NewsLetter';
-import { InfoFooter as Footer } from '../InfoFooter';
+import { Hero } from "./Hero";
+import { Features } from "./Features";
+import { Benefits } from "./Benefits";
+import { InfoFooter as Footer } from "../InfoFooter";
 
 //Export Files for usage in pages/index.tsx document
-export { Hero, Features, Benefits, NewsLetter, Footer };
+export { Hero, Features, Benefits, Footer };
