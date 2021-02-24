eb65c7556d7549cd6477143e9ff66d76f144ecb5
Florian Kulig <florian.kulig@web.de>
2021-02-24 18:49:15 +0100

    added FeedbackForm Component

---
+++ src/components/forms/FeedbackForm.tsx
@@ -0,0 +1,12 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+export const FeedbackForm = ({}) => {
+  return (
+    <>
+      <motion.h1 layoutId="leaveFeedbackHead" className="section-header">
+        Leave your feedback
+      </motion.h1>
+    </>
+  );
+};
