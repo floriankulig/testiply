6e19671b3fcb0c030232c885f8930adfc5a7b151
Florian Kulig <florian.kulig@web.de>
2021-02-24 20:04:53 +0100

    exporting new form components

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -3,12 +3,14 @@ import {
   StyledFormInput,
   StyledMetaInputInfo,
   StyledTextField,
+  StyledTextFieldArea,
   BorderedSVGWrapper,
 } from "./FormInput";
 import { AuthForm } from "./AuthForm";
 import { FormikStepper } from "./FormikStepper";
 import { FormikStep } from "./FormikStep";
 import { FormikTextInput } from "./FormikTextInput";
+import { FormikTextArea } from "./FormikTextArea";
 import { FormikSelectionInput } from "./FormikSelectionInput";
 import { FormikCheckbox } from "./FormikCheckbox";
 import { FeedbackForm } from "./FeedbackForm";
@@ -17,11 +19,13 @@ export {
   SVGWrapper,
   StyledFormInput,
   StyledTextField,
+  StyledTextFieldArea,
   StyledMetaInputInfo,
   AuthForm,
   FormikStepper,
   FormikStep,
   FormikTextInput,
+  FormikTextArea,
   FormikSelectionInput,
   FormikCheckbox,
   BorderedSVGWrapper,
