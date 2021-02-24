720e1f1601251f7f273e90036e06f54e19bfbda9
Florian Kulig <florian.kulig@web.de>
2021-02-24 22:34:18 +0100

    refactored date formatting function

--- src/helpers/getFormattedDate.ts
+++ src/helpers/getFormattedDate.ts
@@ -1,4 +1,11 @@
-const getFormattedDate = (date: Date) =>
-  date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+const getFormattedDate = (date: Date) => {
+  const days = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
+  const month =
+    date.getMonth() + 1 < 10
+      ? `0${date.getMonth() + 1}`
+      : `${date.getMonth() + 1}`;
+
+  return days + "/" + month + "/" + date.getFullYear();
+};
 
 export default getFormattedDate;
