1e1d152af1ac0e2636c97a4cd96ad0b8aa5a3be8
Florian Kulig <florian.kulig@web.de>
2021-03-04 21:45:52 +0100

    cleaned up / refactored for new API

--- src/hooks/useCategory.tsx
+++ src/hooks/useCategory.tsx
@@ -4,15 +4,12 @@ import { getCurrentCategoryFromRoute } from "helpers";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { App, Category } from "ts";
-import { api_url } from "ts/constants";
 
 type CategoryType = Category | null;
 interface ReturnType {
   selectedCategory: CategoryType;
   loading: boolean;
-  hasMoreApps: boolean;
   apps: App[];
-  pageUp: () => void;
 }
 
 export const useCategory = (): ReturnType => {
@@ -30,65 +27,29 @@ export const useCategory = (): ReturnType => {
 
   // Requesting Apps Logic
   const [apps, setApps] = useState<App[]>([]);
-  const [page, setPage] = useState<number>(1);
   const [loading, setLoading] = useState<boolean>(true);
-  const [hasMoreApps, setHasMoreApps] = useState<boolean>(true);
   useEffect(() => {
-    setPage(1);
-    setHasMoreApps(true);
     if (selectedCategory === null) {
       setApps([]);
       return;
     }
     setLoading(true);
-    let query: string = `?category=${selectedCategory.id}`;
-    query +=
-      selectedPlatform.id === "all" ? "" : `&platform[]=${selectedPlatform.id}`;
-
+    const query: string = `?category=${selectedCategory.id}&platform=${selectedPlatform.id}`;
     axios
-      .get(`${api_url}/categoryApps${query}`)
+      .get(`${process.env.API_URL}/getCategoryApps${query}`)
       .then((res) => {
         if (JSON.stringify(res.data.apps) !== JSON.stringify(apps)) {
           setApps(res.data.apps);
         }
-        setHasMoreApps(res.data.metaData.moreApps);
         setLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch(
+        (err) =>
+          err.response.status === 404 &&
+          err.response.data.err === "No Apps found." &&
+          setApps([])
+      );
   }, [selectedCategory, selectedPlatform]);
 
-  useEffect(() => {
-    if (!selectedCategory || page <= 1) return;
-
-    setLoading(true);
-    let query: string = `?category=${selectedCategory.id}&page=${
-      apps.slice(-1).pop()._id
-    }`;
-    query +=
-      selectedPlatform.id === "all" ? "" : `&platform[]=${selectedPlatform.id}`;
-
-    axios
-      .get(`${api_url}/categoryApps${query}`)
-      .then((res) => {
-        // check if new loaded apps are already displayed
-        const newPageApps = res.data.apps.filter(
-          (newApp: App) => !apps.some((oldApp) => oldApp._id === newApp._id)
-        );
-        // merge old apps with newly loaded ones
-        const newApps: App[] = [...apps, ...newPageApps];
-        if (JSON.stringify(newApps) !== JSON.stringify(apps)) {
-          setApps(newApps);
-        }
-        setHasMoreApps(res.data.metaData.moreApps);
-        setLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, [page]);
-
-  const pageUp = (): void => {
-    //handle hasMoreAppsToken
-    setPage(page + 1);
-  };
-
-  return { selectedCategory, loading, apps, pageUp, hasMoreApps };
+  return { selectedCategory, loading, apps };
 };
