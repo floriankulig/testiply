3036d25a40d801a4a80dad09c79c10beba98c6a5
Florian Kulig <florian.kulig@web.de>
2021-03-05 17:20:35 +0100

    adjusted getAppInfo function to new API

--- src/api/getAppInfo.ts
+++ src/api/getAppInfo.ts
@@ -1,19 +1,14 @@
 import axios from "axios";
 import { App } from "ts";
-import { api_url } from "ts/constants";
 
 const getAppInfo = async (id: string) => {
-  const res = await axios.get(`${api_url}/appInfo?id=${id}`);
-  const screenshots = await axios.get(
-    `https://media.beta-app-store.com/apps/screenshots/${id}/config.json`
-  );
-
-  const returnType: App = {
-    ...res.data,
-    screenshots: [...screenshots.data],
+  const res = await axios.get(`${process.env.API_URL}/getApp?appid=${id}`);
+  const returnData: App = {
+    ...res.data.app,
+    screenshots: ["0.webp", "1.webp", "2.webp", "3.webp"],
     _id: id,
   };
-  return returnType;
+  return returnData;
 };
 
 export default getAppInfo;
