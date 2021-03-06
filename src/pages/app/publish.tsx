b7dcf8992e34f32fc7b45c99fc6253e47e9102a7
Florian Kulig <florian.kulig@web.de>
2021-03-06 18:43:12 +0100

    added publish route

---
+++ src/pages/app/publish.tsx
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import { NextPage } from "next";
+import { InfoPageHeader } from "components/InfoPageHeader";
+import { getTextColor } from "helpers";
+import { theme } from "styles";
+import { Footer } from "components/home";
+import { PublishAppForm } from "components/forms";
+import { TabHeader } from "components/TabHeader";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const FormWrapper = styled.div`
+  margin: 8em auto;
+  width: clamp(1px, 90%, 550px);
+  height: 550px;
+`;
+
+const PublishApp: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="favicons/favicon.ico"
+          type="image/x-icon"
+        />
+        <title>Publish your own app | Testiply</title>
+      </Head>
+      <InfoPageHeader
+        className="container-small"
+        style={{
+          color: getTextColor(theme.layoutContentBg),
+        }}
+      />
+      <FormWrapper as={motion.div} animate>
+        <TabHeader as={motion.h1} animate layoutId="publishApp-heading">
+          Publish your app.
+        </TabHeader>
+        <PublishAppForm />
+      </FormWrapper>
+      <Footer />
+    </>
+  );
+};
+
+export default PublishApp;
