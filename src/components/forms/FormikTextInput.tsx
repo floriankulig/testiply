373913424f337eac5a02364b314d8ce8596593e5
Florian Kulig <florian.kulig@web.de>
2021-03-06 18:54:35 +0100

    made svg optional

--- src/components/forms/FormikTextInput.tsx
+++ src/components/forms/FormikTextInput.tsx
@@ -1,32 +1,50 @@
-import { useField } from 'formik'
-import { CSSTransition } from 'react-transition-group';
-import { StyledFormInput, StyledMetaInputInfo, SVGWrapper, StyledTextField } from '.'
+import { useField } from "formik";
+import { CSSTransition } from "react-transition-group";
+import {
+  StyledFormInput,
+  StyledMetaInputInfo,
+  SVGWrapper,
+  StyledTextField,
+} from ".";
 
+export const FormikTextInput = ({
+  svg,
+  svgClickHandler,
+  label,
+  ...restProps
+}: any) => {
+  const [field, meta] = useField(restProps);
+  const showsError: boolean = meta.touched && meta.error ? true : false;
 
-export const FormikTextInput = ({ svg, svgClickHandler, label, ...restProps }: any) => {
-    const [field, meta] = useField(restProps);
-    const showsError: boolean = meta.touched && meta.error ? true : false
-
-    return (
-        <StyledFormInput>
-            <StyledMetaInputInfo>
-                {label}
-                <CSSTransition in={showsError} classNames="error" timeout={250} unmountOnExit>
-                    <span>{meta.error}</span>
-                </CSSTransition>
-            </StyledMetaInputInfo>
-            <StyledTextField hasError={showsError}>
-                <input
-                    {...field} {...restProps}
-                />
-                <SVGWrapper
-                    onClick={() => { !!svgClickHandler && svgClickHandler() }}
-                    onKeyDown={() => { !!svgClickHandler && svgClickHandler() }}
-                    clickable={!!svgClickHandler}
-                >
-                    {svg}
-                </SVGWrapper>
-            </StyledTextField>
-        </StyledFormInput>
-    )
-}
+  return (
+    <StyledFormInput>
+      <StyledMetaInputInfo>
+        {label}
+        <CSSTransition
+          in={showsError}
+          classNames="error"
+          timeout={250}
+          unmountOnExit
+        >
+          <span>{meta.error}</span>
+        </CSSTransition>
+      </StyledMetaInputInfo>
+      <StyledTextField hasError={showsError}>
+        <input {...field} {...restProps} />
+        {svg && (
+          <SVGWrapper
+            onClick={() => {
+              !!svgClickHandler && svgClickHandler();
+            }}
+            onKeyDown={() => {
+              !!svgClickHandler && svgClickHandler();
+            }}
+            clickable={!!svgClickHandler}
+          >
+            {svg}
+          </SVGWrapper>
+        )}
+      </StyledTextField>
+    </StyledFormInput>
+  );
+};
