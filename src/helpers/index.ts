6863840e6c7d29b24836dfbe9f94cfbd44c9de9e
Florian Kulig <florian.kulig@web.de>
2021-03-10 19:35:55 +0100

    added imports/exports for Helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,3 +3,4 @@ export { default as capitalized } from "./capitalized";
 export { default as getCurrentTabFromRoute } from "./getCurrentTabFromRoute";
 export { default as getCurrentCategoryFromRoute } from "./getCurrentCategoryFromRoute";
 export { default as getFormattedDate } from "./getFormattedDate";
+export { default as oppositeFormType } from "./oppositeFormType";
