4c99cd390f954d7a89456c68a402eea9c6911996
Florian Kulig <florian.kulig@web.de>
2021-03-13 22:23:08 +0100

    redirecting from today tab

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,20 +1,33 @@
-import { Layout } from 'components/layouts'
-import { TabHeader } from 'components/TabHeader'
-import Head from 'next/head'
+import { Layout } from "components/layouts";
+import { TabHeader } from "components/TabHeader";
+import { NextPageContext } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 const Today = () => {
-    return (
-        <>
-            <Head>
-                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-            </Head>
-            <TabHeader>
-                Today
-            </TabHeader>
-        </>
-    )
-}
+  const router = useRouter();
+  // Make sure we're in the browser
+  if (typeof window !== "undefined") {
+    router.push("/store/categories");
+  }
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <TabHeader>Today</TabHeader>
+    </>
+  );
+};
 
-Today.Layout = Layout
+Today.Layout = Layout;
 
-export default Today
+Today.getInitialProps = (ctx: NextPageContext) => {
+  if (ctx.res) {
+    ctx.res.writeHead(302, { Location: "/store/categories" });
+    ctx.res.end();
+  }
+  return {};
+};
+
+export default Today;
