8122a33f9d323017b304f0a9c58a094c2c88a0b8
Florian Kulig <florian.kulig@web.de>
2021-03-15 18:45:18 +0100

    added route for upgrading to dev account

---
+++ src/pages/dev/upgrade.tsx
@@ -0,0 +1,61 @@
+import Head from "next/head";
+import { NextPage, NextPageContext } from "next";
+import { InfoPageHeader } from "components/InfoPageHeader";
+import { getTextColor } from "helpers";
+import { theme } from "styles";
+import { Footer } from "components/home";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { DevAuthForm } from "components/auth";
+import { useRouter } from "next/router";
+
+interface DevUpgradeProps {
+  hasUser: boolean;
+}
+
+const FormWrapper = styled.div`
+  margin: 8em auto;
+  width: clamp(1px, 90%, 700px);
+  /* height: 550px; */
+`;
+
+const DevUpgrade: NextPage<DevUpgradeProps> = ({ hasUser }) => {
+  const router = useRouter();
+  // Make sure we're in the browser
+  if (typeof window !== "undefined" && !hasUser) {
+    router.push("/login");
+  }
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="favicons/favicon.ico"
+          type="image/x-icon"
+        />
+        <title>Upgrade to a publisher account| Testiply</title>
+      </Head>
+      <InfoPageHeader
+        className="container-small"
+        style={{
+          color: getTextColor(theme.layoutContentBg),
+        }}
+      />
+      <FormWrapper as={motion.div} animate>
+        <DevAuthForm hasUserRegistered={hasUser} />
+      </FormWrapper>
+      <Footer />
+    </>
+  );
+};
+
+DevUpgrade.getInitialProps = ({ res, req }: NextPageContext) => {
+  const hasUser = !!req?.headers.cookie?.slice(3);
+  if (res && !hasUser) {
+    res.writeHead(302, { Location: "/login" });
+    res.end();
+  }
+  return { hasUser };
+};
+
+export default DevUpgrade;
