5aeadfaa8731200d5462e8e93ada9f7eb6026b30
Florian Kulig <florian.kulig@web.de>
2021-03-15 17:45:24 +0100

    removed screenshot array from return

--- src/api/getAppInfo.ts
+++ src/api/getAppInfo.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { App } from "ts";
 
-const getAppInfo = async (id: string) => {
+const getAppInfo = async (id: string): Promise<App> => {
   const res = await axios.get(
     `${process.env.NEXT_PUBLIC_API_URL}/getApp?appid=${id}`
   );
   const returnData: App = {
     ...res.data.app,
-    screenshots: ["0.webp", "1.webp", "2.webp", "3.webp"],
     _id: id,
   };
   return returnData;
