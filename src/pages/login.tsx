123854381692cb9a9120344850923688bf0fc790
Florian Kulig <florian.kulig@web.de>
2021-03-15 19:00:05 +0100

    validating auth status for auth forms

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,19 @@
 import { TesterAuthForm } from "components/auth";
 import { AuthLayout } from "components/layouts";
-import { NextPage } from "next";
+import { NextPage, NextPageContext } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
-const Login: NextPage = () => {
+interface LoginProps {
+  hasUser: boolean;
+}
+
+const Login: NextPage<LoginProps> = ({ hasUser }) => {
+  const router = useRouter();
+  if (typeof window !== "undefined" && hasUser) {
+    router.push("/store");
+  }
+
   return (
     <>
       <Head>
@@ -14,6 +24,15 @@ const Login: NextPage = () => {
       </AuthLayout>
     </>
   );
+};
+
+Login.getInitialProps = ({ res, req }: NextPageContext) => {
+  const hasUser = !!req?.headers.cookie?.slice(3);
+  if (res && hasUser) {
+    res.writeHead(302, { Location: "/store" });
+    res.end();
+  }
+  return { hasUser };
 };
 
 export default Login;
