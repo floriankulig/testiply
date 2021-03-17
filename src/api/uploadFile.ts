65185356dfd66ba24ea27c53c2e4358367cee966
Florian Kulig <florian.kulig@web.de>
2021-03-17 21:02:18 +0100

    added upload file functoin

---
+++ src/api/uploadFile.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const uploadFile = (
+  file: File,
+  i: number,
+  setErrorMessage: React.Dispatch<React.SetStateAction<string>>
+): Promise<string> =>
+  new Promise<string>((res, rej) => {
+    const formData = new FormData();
+    let url: string;
+
+    formData.append("uploaded", file, `${i + 1}.png`);
+
+    axios
+      .post(`${process.env.NEXT_PUBLIC_API_URL}/upload`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => (url = res.data.url))
+      .catch((err) => setErrorMessage(err.response.data.err));
+    return url;
+  });
+
+export default uploadFile;
