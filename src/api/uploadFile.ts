c87693dc5b1a0495ea8cc6067193ea3f6a26239f
Florian Kulig <florian.kulig@web.de>
2021-03-21 17:17:11 +0100

    returning nothing from upload

--- src/api/uploadFile.ts
+++ src/api/uploadFile.ts
@@ -1,27 +1,25 @@
 import axios from "axios";
 
-const uploadFile = (
+const uploadFile = async (
   file: File,
   name: string,
   setErrorMessage: React.Dispatch<React.SetStateAction<string>>,
   appId: string
-): Promise<string> =>
-  new Promise<string>((res, rej) => {
-    const formData = new FormData();
-    let url: string;
+): Promise<void> => {
+  const formData = new FormData();
+  let url: string;
 
-    formData.append("uploaded", file, `${name}.png`);
-    formData.append("appid", appId);
+  formData.append("uploaded", file, `${name}.png`);
+  formData.append("appid", appId);
 
-    axios
-      .post(`${process.env.NEXT_PUBLIC_API_URL}/upload`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((res) => (url = res.data.url))
-      .catch((err) => setErrorMessage(err.response.data.err));
-    return url;
-  });
+  await axios
+    .post(`${process.env.NEXT_PUBLIC_API_URL}/upload`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((res) => (url = res.data.url))
+    .catch((err) => setErrorMessage(err.response.data.err));
+};
 
 export default uploadFile;
