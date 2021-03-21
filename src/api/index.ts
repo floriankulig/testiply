ab9224df93fa2bfe7a4d42ce0a3452263d46855b
Florian Kulig <florian.kulig@web.de>
2021-03-21 17:17:26 +0100

    added imports/exports for new functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export { default as postData } from "./postData";
 export { default as getAppInfo } from "./getAppInfo";
 export { default as uploadFile } from "./uploadFile";
+export { default as createApp } from "./createApp";
