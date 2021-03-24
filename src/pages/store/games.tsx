33a6042889de17d06480258361ea0f3583da03cc
Florian Kulig <florian.kulig@web.de>
2021-03-24 17:55:20 +0100

    displaying disabled tab in games

--- src/pages/store/games.tsx
+++ src/pages/store/games.tsx
@@ -1,20 +1,22 @@
-import { Layout } from 'components/layouts'
-import { TabHeader } from 'components/TabHeader'
-import Head from 'next/head'
+import { AppGrid, Layout } from "components/layouts";
+import { TabCurrentlyWorkedOn } from "components/store/TabCurrentlyWorkedOn";
+import { TabHeader } from "components/TabHeader";
+import Head from "next/head";
 
 const Games = () => {
-    return (
-        <>
-            <Head>
-                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-            </Head>
-            <TabHeader>
-                Games
-            </TabHeader>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <TabHeader>Games</TabHeader>
+      <AppGrid>
+        <TabCurrentlyWorkedOn />
+      </AppGrid>
+    </>
+  );
+};
 
-Games.Layout = Layout
+Games.Layout = Layout;
 
-export default Games
+export default Games;
