b7d2bc62521dbdd98044310cc7cee2a82e528983
Florian Kulig <florian.kulig@web.de>
2021-03-24 17:39:15 +0100

    displaying app grid in apps tab

--- src/pages/store/apps.tsx
+++ src/pages/store/apps.tsx
@@ -1,21 +1,48 @@
-import { Layout } from 'components/layouts'
-import { TabHeader } from 'components/TabHeader'
-import Head from 'next/head'
+import { AppGrid, Layout } from "components/layouts";
+import { Loading } from "components/Loading";
+import { AppTile, NoAppsView } from "components/store";
+import { TabHeader } from "components/TabHeader";
+import { useApps } from "hooks";
+import Head from "next/head";
 
 const Apps = () => {
-    return (
-        <>
-            <Head>
-                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-            </Head>
-            <TabHeader>
-                Apps
-            </TabHeader>
-        </>
-    )
-}
+  const { filteredApps, loading } = useApps();
 
-Apps.Layout = Layout
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <TabHeader>Apps</TabHeader>
+      <AppGrid>
+        {!filteredApps.length ? (
+          <NoAppsView hasApps={!!filteredApps.length}>
+            No Apps uploaded yet.
+          </NoAppsView>
+        ) : (
+          <>
+            {filteredApps.map((app, i) => (
+              <AppTile
+                key={app._id}
+                style={{ animationDelay: `${i * 15}ms` }}
+                appInfo={app}
+              />
+            ))}
+            <div className="full-grid-width">
+              {loading && (
+                <h2 className="loading">
+                  Loading
+                  <Loading size={60} />
+                </h2>
+              )}
+            </div>
+          </>
+        )}
+      </AppGrid>
+    </>
+  );
+};
 
+Apps.Layout = Layout;
 
-export default Apps
+export default Apps;
