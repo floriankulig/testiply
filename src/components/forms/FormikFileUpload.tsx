e1aafc5ddbed3a83c1ed9d6cd44640ff2c079b27
Florian Kulig <florian.kulig@web.de>
2021-03-24 20:02:11 +0100

    added styles for file uploads

--- src/components/forms/FormikFileUpload.tsx
+++ src/components/forms/FormikFileUpload.tsx
@@ -1,13 +1,46 @@
 import { Button } from "components/Button";
 import { useField } from "formik";
+import { darken, rgba } from "polished";
 import { useCallback, useEffect, useState } from "react";
 import { FileRejection, useDropzone } from "react-dropzone";
+import { CSSTransition } from "react-transition-group";
+import styled from "styled-components";
+import { StyledMetaInputInfo } from ".";
+import { StyledFormInput } from "./FormInput";
+
+const DashedDropzone = styled.div<{ hasError: boolean }>`
+  padding: 3em;
+  border-radius: 0.8em;
+  border: 3px
+    ${(p) => (p.hasError ? "red" : darken(0.1, p.theme.layoutContentBg))} dashed;
+  background: ${(p) =>
+    p.hasError ? rgba(255, 0, 0, 0.05) : "var(--layout-content-background)"};
+  display: grid;
+  place-items: center;
+  margin: 0.2em 0 1.5em;
+  cursor: pointer;
+  transition: 0.5s;
+  transition-property: border background;
+`;
+
+const StyledFileItems = styled.ul`
+  margin-top: 0.5em;
+`;
+
+const FileItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 0.25em;
+`;
 
 export const FormikFileUpload: React.FC<{
   name: string;
   maxFiles: number;
-}> = ({ name, maxFiles }) => {
+  label: string;
+}> = ({ name, maxFiles, label }) => {
   const [field, meta, helpers] = useField(name);
+  const showsError: boolean = meta.touched && meta.error ? true : false;
 
   const [files, setFiles] = useState<File[]>(field.value);
   const onDrop = useCallback((accFiles: File[], rejFiles: FileRejection[]) => {
@@ -35,34 +68,47 @@ export const FormikFileUpload: React.FC<{
       return;
     }
     helpers.setValue(files);
-    helpers.setTouched(true);
+    // helpers.setTouched(true);
   }, [files]);
 
   return (
-    <>
+    <StyledFormInput>
+      <StyledMetaInputInfo>
+        {label + ` ${field.value.length}/${maxFiles}`}
+        <CSSTransition
+          in={showsError}
+          classNames="error"
+          timeout={250}
+          unmountOnExit
+        >
+          <span>{meta.error}</span>
+        </CSSTransition>
+      </StyledMetaInputInfo>
       {files.length < maxFiles && (
-        <div {...getRootProps()}>
+        <DashedDropzone {...getRootProps()} hasError={showsError}>
           <input {...getInputProps()} name={name} />
 
           <p>Drag 'n' drop some files here, or click to select files</p>
-        </div>
+        </DashedDropzone>
       )}
-      {meta.touched && meta.error && (
-        <span style={{ color: "red" }}>{meta.error}</span>
+      {!!files.length && (
+        <StyledFileItems>
+          {files?.map((file) => (
+            <FileItem>
+              {file.name.slice(0, 45)}
+              {file.name.length > 45 ? "..." : " "}
+              <Button
+                color="red"
+                transparent
+                type="button"
+                onClick={() => onDelete(file)}
+              >
+                Delete
+              </Button>
+            </FileItem>
+          ))}
+        </StyledFileItems>
       )}
-      {files?.map((file) => (
-        <div>
-          {file.name}{" "}
-          <Button
-            color="red"
-            transparent
-            type="button"
-            onClick={() => onDelete(file)}
-          >
-            Delete
-          </Button>
-        </div>
-      ))}
-    </>
+    </StyledFormInput>
   );
 };
