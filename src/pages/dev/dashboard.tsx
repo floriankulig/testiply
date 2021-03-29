327ef6aa3063093c1e20f5e32e5b494b1276b9dd
Florian Kulig <florian.kulig@web.de>
2021-03-29 18:06:42 +0200

    added tab being worked on visual to dashboard tab

--- src/pages/dev/dashboard.tsx
+++ src/pages/dev/dashboard.tsx
@@ -1,20 +1,21 @@
-import { Layout } from "components/layouts"
-import { TabHeader } from "components/TabHeader"
-import Head from "next/head"
+import { AppGrid, Layout } from "components/layouts";
+import { TabCurrentlyWorkedOn } from "components/store/TabCurrentlyWorkedOn";
+import { TabHeader } from "components/TabHeader";
+import Head from "next/head";
 
 const DevDashboard = () => {
-    return (
-        <>
-            <Head>
-                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-            </Head>
-            <TabHeader>
-                Dashboard
-            </TabHeader>
-        </>
-    )
-}
-DevDashboard.Layout = Layout
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <TabHeader>Dashboard</TabHeader>
+      <AppGrid>
+        <TabCurrentlyWorkedOn />
+      </AppGrid>
+    </>
+  );
+};
+DevDashboard.Layout = Layout;
 
-
-export default DevDashboard
+export default DevDashboard;
