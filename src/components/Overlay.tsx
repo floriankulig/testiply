16b4faec6a257ed564d943e3d38aadae4df1d369
Florian Kulig <florian.kulig@web.de>
2021-04-02 20:18:26 +0200

    moving up overlay's z-index

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -10,7 +10,7 @@ const StyledOverlay = styled.div`
   left: 50%;
   width: 100vw;
   height: 100vh;
-  z-index: 99;
+  z-index: 99999;
   transform: translate(-50%, -50%);
   background: ${({ theme }) => rgba(theme.navy, 0.3)};
 `;
