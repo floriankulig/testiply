392138c2bafc15ef2b1a902c1430fed5de7167ac
Florian Kulig <florian.kulig@web.de>
2021-04-11 21:33:35 +0200

    added imports/exports for new variants

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,5 @@
+import { fadeInOutVariants, fadeUpVariants } from "./variants";
+
+export { fadeInOutVariants, fadeUpVariants };
 export { default as GlobalStyle } from "./GlobalStyle";
 export { default as theme } from "./theme";
