99df85278925a5755a2a55c0a483a51e1bcefffe
Florian Kulig <florian.kulig@web.de>
2021-04-13 21:28:32 +0200

    managing new imports/exports

--- src/components/store/index.ts
+++ src/components/store/index.ts
@@ -1,4 +1,17 @@
 import { NoAppsView } from "./NoAppsView";
 import { AppTile } from "./AppTile";
+import {
+  StyledAppRow,
+  StyledAppRowHeader,
+  StyledAppStoreRow,
+  ViewAllButton,
+} from "./AppRow";
 
-export { NoAppsView, AppTile };
+export {
+  NoAppsView,
+  AppTile,
+  StyledAppRow,
+  StyledAppRowHeader,
+  ViewAllButton,
+  StyledAppStoreRow,
+};
