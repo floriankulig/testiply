f7904d13a3a91a7e163e077b34e4c3eb2222fbfd
Florian Kulig <florian.kulig@web.de>
2021-04-16 23:12:45 +0200

    using new types in hooks

--- src/pages/store/games.tsx
+++ src/pages/store/games.tsx
@@ -3,18 +3,21 @@ import { TabCurrentlyWorkedOn } from "components/store/TabCurrentlyWorkedOn";
 import { TabHeader } from "components/TabHeader";
 import Head from "next/head";
 import { GamesRows } from "components/store";
+import { useGamesApps } from "hooks";
 
 const Games = () => {
+  const { apps } = useGamesApps();
+
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
       <TabHeader>Games</TabHeader>
-      <AppGrid>
+      {/* <AppGrid>
         <TabCurrentlyWorkedOn />
-      </AppGrid>
-      {/* <GamesRows /> */}
+      </AppGrid> */}
+      <GamesRows apps={apps} />
     </>
   );
 };
