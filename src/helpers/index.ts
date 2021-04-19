a6e7d64f674a8623f3754b9121d3f7eb10c4c25e
Florian Kulig <florian.kulig@web.de>
2021-04-19 19:53:19 +0200

    added new imports/exports

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,5 +2,6 @@ export { default as getTextColor } from "./getTextColor";
 export { default as capitalized } from "./capitalized";
 export { default as getCurrentTabFromRoute } from "./getCurrentTabFromRoute";
 export { default as getCurrentCategoryFromRoute } from "./getCurrentCategoryFromRoute";
+export { default as getCurrentGameCategoryFromRoute } from "./getCurrentGameCategoryFromRoute";
 export { default as getFormattedDate } from "./getFormattedDate";
 export { default as oppositeFormType } from "./oppositeFormType";
