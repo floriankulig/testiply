303277e4ca3886b5e74f413eb0ecb50b02291e37
Florian Kulig <florian.kulig@web.de>
2021-04-21 21:28:12 +0200

    added new imports/exports for api-setup function

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,4 @@ export { default as postData } from "./postData";
 export { default as getAppInfo } from "./getAppInfo";
 export { default as uploadFile } from "./uploadFile";
 export { default as createApp } from "./createApp";
+export { default as setAPIHeaders } from "./setAPIHeaders";
