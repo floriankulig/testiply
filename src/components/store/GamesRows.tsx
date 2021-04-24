58ce634797d4fc7d1da1a874dcb8bd1786958768
Florian Kulig <florian.kulig@web.de>
2021-04-24 20:46:50 +0200

    dynamically mapping over app rows

--- src/components/store/GamesRows.tsx
+++ src/components/store/GamesRows.tsx
@@ -1,9 +1,18 @@
+import { AppGrid } from "components/layouts";
 import { Loading } from "components/Loading";
 import { MenuTransition } from "components/MenuTransition";
+import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import React from "react";
 import { FiClock, FiDownload, FiStar } from "react-icons/fi";
-import { App, AppRowCategory, GameRowApps } from "ts";
+import { CSSTransition } from "react-transition-group";
+import {
+  App,
+  AppRowCategory,
+  AppRowCategoryID,
+  GameRowApps,
+  gameRowCategories,
+} from "ts";
 import { AppTile } from ".";
 import {
   StyledAppRowHeader,
@@ -26,62 +35,91 @@ export const GamesRows: React.FC<GamesRowsProps> = ({
   selectedCategory,
   loading,
 }) => {
-  if (loading) {
-    return (
-      <div className="full-grid-width">
-        <h2 className="loading">
-          Loading
-          <Loading size={60} />
-        </h2>
-      </div>
-    );
-  }
-  if (!initialApps?.latest[0] || !initialApps) {
-    return <NoAppsView hasApps={false}>No Games uploaded yet.</NoAppsView>;
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="full-grid-width">
+  //       <h2 className="loading">
+  //         Loading
+  //         <Loading size={60} />
+  //       </h2>
+  //     </div>
+  //   );
+  // }
+  // if (!initialApps?.latest[0] || !initialApps) {
+  //   return <NoAppsView hasApps={false}>No Games uploaded yet.</NoAppsView>;
+  // }
   const router = useRouter();
-  const { latest, rating, downloads } = initialApps;
 
   return (
     <MenuTransition>
-      <StyledAppStoreRow>
-        <StyledAppRowHeader>
-          <h2 className="tab-name">
-            <FiClock />
-            Latest
-          </h2>
-          <ViewAllButton
-            clickHandler={() => router.push("/store/games?category=latest")}
-          />
-        </StyledAppRowHeader>
-        <StyledAppRow>
-          {latest?.map((app) => (
-            <AppTile key={app._id} appInfo={app} />
-          ))}
-        </StyledAppRow>
-      </StyledAppStoreRow>
-      <StyledAppStoreRow>
-        <StyledAppRowHeader>
-          <h2 className="tab-name">
-            <FiStar />
-            Best rating
-          </h2>
-          <ViewAllButton
-            clickHandler={() => router.push("/store/games?category=rating")}
-          />
-        </StyledAppRowHeader>
-      </StyledAppStoreRow>
-      <StyledAppStoreRow>
-        <StyledAppRowHeader>
-          <h2 className="tab-name">
-            <FiDownload />
-            Most Downloads
-          </h2>
-          <ViewAllButton
-            clickHandler={() => router.push("/store/games?category=downloads")}
-          />
-        </StyledAppRowHeader>
-      </StyledAppStoreRow>
+      <CSSTransition
+        in={!selectedCategory}
+        classNames="menu-primary"
+        timeout={400}
+        unmountOnExit
+        appear
+      >
+        <div>
+          {!!initialApps &&
+            Object.keys(initialApps)?.map((key: AppRowCategoryID) => {
+              const row: AppRowCategory = gameRowCategories.find(
+                (cat) => cat.id === key
+              );
+              const rowApps: App[] = initialApps[key];
+
+              return (
+                <StyledAppStoreRow key={row.id}>
+                  <StyledAppRowHeader>
+                    <h2 className="tab-name">
+                      <row.icon />
+                      {row.displayName}
+                    </h2>
+                    <ViewAllButton
+                      clickHandler={() =>
+                        router.push(`/store/games?category=${row.id}`)
+                      }
+                    />
+                  </StyledAppRowHeader>
+                  <StyledAppRow>
+                    {rowApps?.map((app) => (
+                      <AppTile key={app._id} appInfo={app} />
+                    ))}
+                  </StyledAppRow>
+                </StyledAppStoreRow>
+              );
+            })}
+          {loading && (
+            <div className="full-grid-width">
+              <h2 className="loading">
+                Loading
+                <Loading size={60} />
+              </h2>
+            </div>
+          )}
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={!!selectedCategory}
+        classNames="menu-secondary"
+        timeout={400}
+        unmountOnExit
+      >
+        <AppGrid>
+          <AnimatePresence>
+            {apps.map((app) => (
+              <AppTile key={app._id} appInfo={app} />
+            ))}
+          </AnimatePresence>
+          <div className="full-grid-width">
+            {loading && (
+              <h2 className="loading">
+                Loading
+                <Loading size={60} />
+              </h2>
+            )}
+          </div>
+        </AppGrid>
+      </CSSTransition>
     </MenuTransition>
   );
 };
