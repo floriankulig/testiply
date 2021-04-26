16953930ff2ebe33d2ad29311e78b95483e4941d
Florian Kulig <florian.kulig@web.de>
2021-04-26 18:45:38 +0200

    added new animation variant

--- src/styles/variants.ts
+++ src/styles/variants.ts
@@ -1,5 +1,11 @@
 import { Variants } from "framer-motion";
 
+export const scaleInOutVariants: Variants = {
+  open: { scale: 1, opacity: 1 },
+  closed: { scale: 0, opacity: 0 },
+  show: { scale: 1, opacity: 1 },
+  hidden: { scale: 0, opacity: 0 },
+};
 export const fadeInOutVariants: Variants = {
   open: {
     opacity: 1,
