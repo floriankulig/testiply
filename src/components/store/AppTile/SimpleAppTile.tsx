713c9a26e1aa92d6f3d4fafd62ff43d6d9a899b8
Florian Kulig <florian.kulig@web.de>
2021-05-13 19:17:02 +0200

    added dynamic styles for mobile/desktop

--- src/components/store/AppTile/SimpleAppTile.tsx
+++ src/components/store/AppTile/SimpleAppTile.tsx
@@ -1,5 +1,6 @@
 import { MockAppDetailView } from "components/appDetail";
 import { AnimatePresence, motion } from "framer-motion";
+import { useIsMobile } from "hooks";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { rgba } from "polished";
@@ -8,18 +9,24 @@ import { FaStar } from "react-icons/fa";
 import styled from "styled-components";
 import { AppTileProps } from "./AppTile";
 
-const StyledAppTile = styled.li`
+interface MobileProps {
+  isMobile: boolean;
+}
+
+const StyledAppTile = styled.li<MobileProps>`
+  --size: ${(p) => (p.isMobile ? "110px" : "130px")};
+
   display: flex;
   flex-direction: column;
-  max-width: 110px;
+  max-width: var(--size);
 `;
 
-const StyledAppTileMeta = styled.div`
+const StyledAppTileMeta = styled.div<MobileProps>`
   width: 100%;
   padding-left: 0.25em;
   position: relative;
   h3 {
-    font-size: 1.05rem;
+    font-size: ${(p) => (p.isMobile ? "1.05rem" : "1.15rem")};
     line-height: 1.2;
     margin: 0.3em 0 0.2em;
     color: ${({ theme }) => rgba(theme.navy, 0.95)};
@@ -32,7 +39,8 @@ const StyledAppTileMeta = styled.div`
   }
 
   span {
-    font-size: 0.95rem;
+    font-size: ${(p) => (p.isMobile ? "0.95rem" : "1rem")};
+
     color: ${({ theme }) => rgba(theme.navy, 0.7)};
     svg {
       font-size: 0.7rem;
@@ -45,11 +53,13 @@ const StyledAppTileMeta = styled.div`
   }
 `;
 
-const IconWrapper = styled.div`
-  height: 110px;
-  width: 110px;
-  min-height: 110px;
-  min-width: 110px;
+const IconWrapper = styled.div<MobileProps>`
+  --size: ${(p) => (p.isMobile ? "110px" : "130px")};
+
+  height: var(--size);
+  width: var(--size);
+  min-height: var(--size);
+  min-width: var(--size);
   box-shadow: 0px 2px 6px ${({ theme }) => rgba(theme.navy, 0.08)};
   border-radius: 20%;
   user-select: none;
@@ -79,6 +89,7 @@ export const SimpleAppTile: React.FC<AppTileProps> = ({
 }) => {
   const [detailOpened, setDetailOpened] = useState<boolean>(false);
   const router = useRouter();
+  const isMobile = useIsMobile(1080);
   const iconURL = `${process.env.NEXT_PUBLIC_API_URL}/static/${_id}/icon.png`;
 
   const layID = _id + customID;
@@ -96,23 +107,25 @@ export const SimpleAppTile: React.FC<AppTileProps> = ({
         as={motion.li}
         layout
         layoutId={`appTile-${layID}`}
+        isMobile={isMobile}
       >
         <IconWrapper
           onTap={handleClick}
           as={motion.div}
           layoutId={`appIcon-${layID}`}
           style={{ borderRadius: "20%" }}
+          isMobile={isMobile}
         >
           <div className="image-cursor-overlay"></div>
           <Image
-            width={110}
-            height={110}
+            width={130}
+            height={130}
             src={iconURL}
             className="icon"
             alt={`${name} app icon`}
           />
         </IconWrapper>
-        <StyledAppTileMeta>
+        <StyledAppTileMeta isMobile={isMobile}>
           <motion.h3 layoutId={`appTitle-${layID}`}>{name}</motion.h3>
           <span>
             {rating.total} <FaStar />
