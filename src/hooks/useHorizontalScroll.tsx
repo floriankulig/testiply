976e34b9ceace76e9bc329eca69869b08ea91b40
Florian Kulig <florian.kulig@web.de>
2021-05-13 21:24:35 +0200

    added hook for horizontal scrolling

---
+++ src/hooks/useHorizontalScroll.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export const useHorizontalScroll = (
+  parentContainer: HTMLElement,
+  scrollingContainer: HTMLElement
+) => {
+  const [scrollable, setScrollable] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!parentContainer || !scrollingContainer) return;
+
+    const onResize = () => {
+      setScrollable(
+        scrollingContainer.offsetWidth > parentContainer.offsetWidth
+      );
+    };
+
+    setScrollable(scrollingContainer.offsetWidth > parentContainer.offsetWidth);
+
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, [parentContainer, scrollingContainer]);
+  return { scrollable };
+};
