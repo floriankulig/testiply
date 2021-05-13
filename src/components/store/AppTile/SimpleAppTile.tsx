0d532dd4da44db3d4cb8c2eb36d3e9f62a0b156c
Florian Kulig <florian.kulig@web.de>
2021-05-13 18:34:30 +0200

    added version 1 of SimpleAppTile

--- src/components/store/AppTile/SimpleAppTile.tsx
+++ src/components/store/AppTile/SimpleAppTile.tsx
@@ -5,35 +5,78 @@ import Image from "next/image";
 import Link from "next/link";
 import { rgba } from "polished";
 import { useEffect, useState } from "react";
+import { FaStar } from "react-icons/fa";
 import styled from "styled-components";
 import { AppTileProps } from "./AppTile";
 
 const StyledAppTile = styled.li`
   display: flex;
+  flex-direction: column;
+  max-width: 110px;
+`;
+
+const StyledAppTileMeta = styled.div`
+  width: 100%;
+  padding-left: 0.25em;
+  position: relative;
+  h3 {
+    font-size: 1.05rem;
+    line-height: 1.2;
+    margin: 0.3em 0;
+    color: ${({ theme }) => rgba(theme.navy, 0.95)};
+    font-weight: normal;
+    display: -webkit-box;
+    word-wrap: break;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+
+  span {
+    font-size: 0.95rem;
+    color: ${({ theme }) => rgba(theme.navy, 0.7)};
+    svg {
+      font-size: 0.7rem;
+    }
+  }
+
+  p.invis-for-anim {
+    position: absolute;
+    top: 0.3em;
+  }
 `;
 
 const IconWrapper = styled.div`
-  height: 100px;
-  width: 100px;
-  min-height: 100px;
-  min-width: 100px;
-  box-shadow: 0px 0px 20px ${({ theme }) => rgba(theme.navy, 0.05)};
+  height: 110px;
+  width: 110px;
+  min-height: 110px;
+  min-width: 110px;
+  box-shadow: 0px 2px 6px ${({ theme }) => rgba(theme.navy, 0.08)};
   border-radius: 20%;
   user-select: none;
+  position: relative;
   .icon {
+    pointer-events: none;
     height: 100%;
     width: 100%;
     border-radius: 20%;
     object-fit: cover;
   }
+
+  .image-cursor-overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    cursor: pointer;
+  }
 `;
 
 export const SimpleAppTile: React.FC<AppTileProps> = ({
-  appInfo: { name, description, _id, devName, rating },
+  appInfo: { name, _id, devName, rating },
   className,
   style,
   customID = "",
-  simple,
 }) => {
   const [detailOpened, setDetailOpened] = useState<boolean>(false);
   const iconURL = `${process.env.NEXT_PUBLIC_API_URL}/static/${_id}/icon.png`;
@@ -42,20 +85,44 @@ export const SimpleAppTile: React.FC<AppTileProps> = ({
 
   return (
     <>
-      <StyledAppTile>
-        <IconWrapper
-          as={motion.div}
-          layoutId={`appIcon-${layID}`}
-          style={{ borderRadius: "25%" }}
-        >
-          <Image
-            width={100}
-            height={100}
-            src={iconURL}
-            className="icon"
-            alt={`${name} app icon`}
-          />
-        </IconWrapper>
+      <StyledAppTile
+        className={className}
+        style={style}
+        as={motion.li}
+        layout
+        layoutId={`appTile-${layID}`}
+      >
+        <Link href={`/app/${_id}`}>
+          <IconWrapper
+            onClick={() => setDetailOpened(true)}
+            onKeyDown={() => setDetailOpened(true)}
+            as={motion.div}
+            layoutId={`appIcon-${layID}`}
+            style={{ borderRadius: "20%" }}
+          >
+            <div className="image-cursor-overlay"></div>
+            <Image
+              width={110}
+              height={110}
+              src={iconURL}
+              className="icon"
+              alt={`${name} app icon`}
+            />
+          </IconWrapper>
+        </Link>
+        <StyledAppTileMeta>
+          <motion.h3 layoutId={`appTitle-${layID}`}>{name}</motion.h3>
+          <span>
+            {rating.total} <FaStar />
+          </span>
+          <motion.p
+            className="invis-for-anim"
+            layoutId={`appDevName-${layID}`}
+            style={{ visibility: "hidden" }}
+          >
+            {devName}
+          </motion.p>
+        </StyledAppTileMeta>
       </StyledAppTile>
       <AnimatePresence>
         {detailOpened && (
