e2eff5249c2717722a6a30d0b38c0badc49ef381
Florian Kulig <florian.kulig@web.de>
2021-05-25 23:14:45 +0200

    giviing default value for tester users

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -98,7 +98,7 @@ export const Header: React.FC<HeaderProps> = ({
       {isMobile && <Burger setSidebarOpen={setSidebarOpen} />}
       <Searchbar />
       <StyledMenus>
-        {currentUser?.name}
+        {currentUser?.name || ""}
         <UserMenu />
       </StyledMenus>
     </StyledHeader>
