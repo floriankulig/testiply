18c12a2cd2514cb3a99a0bd8dd33bad215e8bdab
Florian Kulig <florian.kulig@web.de>
2021-12-30 21:37:10 +0100

    Added Wrapper to authorise users in dev tabs

---
+++ src/components/auth/DevAuthorisation.tsx
@@ -0,0 +1,29 @@
+import { useAuthValue } from "context";
+import { useRouter } from "next/router";
+import { Fragment, useEffect } from "react";
+
+interface DevAuthorisationProps {
+  children: React.ReactNode;
+  serverSideDev?: boolean;
+  serverSideLoggedIn?: boolean;
+}
+
+export const DevAuthorisation: React.FC<DevAuthorisationProps> = ({
+  children,
+  serverSideDev,
+  serverSideLoggedIn,
+}) => {
+  const { currentUser, canHaveUser } = useAuthValue();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log({ canHaveUser });
+    console.log({ currentUser });
+    if (canHaveUser) {
+      if (!currentUser) router.push("/store");
+      else if (!currentUser.isDev) router.push("/dev/upgrade");
+    }
+  }, [currentUser, canHaveUser]);
+
+  return <Fragment>{children}</Fragment>;
+};
