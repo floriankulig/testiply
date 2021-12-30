69a715be3a4b54680918d516cb32a961aafc513e
Florian Kulig <florian.kulig@web.de>
2021-12-30 21:37:23 +0100

    added imports/exports for new wrapper

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,5 +1,6 @@
 import { TesterAuthForm } from "./TesterAuthForm";
 import { DevAuthForm } from "./DevAuthForm";
 import { AuthInfoSidebar } from "./AuthInfoSidebar";
+import { DevAuthorisation } from "./DevAuthorisation";
 
-export { TesterAuthForm, AuthInfoSidebar, DevAuthForm };
+export { TesterAuthForm, AuthInfoSidebar, DevAuthForm, DevAuthorisation };
