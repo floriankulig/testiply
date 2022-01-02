ca1fa0e66f8bafa8ef220343d346935c63269c0b
Florian Kulig <florian.kulig@web.de>
2022-01-02 22:54:12 +0100

    gaining server side information

--- src/pages/dev/apps.tsx
+++ src/pages/dev/apps.tsx
@@ -1,21 +1,74 @@
+import axios from "axios";
+import { DevAuthorisation } from "components/auth";
+import { DevAppsRows } from "components/dev";
 import { AppGrid, Layout } from "components/layouts";
-import { TabCurrentlyWorkedOn } from "components/store/TabCurrentlyWorkedOn";
+import { NoAppsView } from "components/store";
 import { TabHeader } from "components/TabHeader";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import Head from "next/head";
+import { App } from "ts";
 
-const DevApps = () => {
+interface DevAppsProps {
+  apps: App[];
+  hasUser: boolean;
+  isDev: boolean;
+}
+
+const DevApps = ({ apps, hasUser, isDev }: DevAppsProps) => {
+  console.log({ apps });
   return (
-    <>
+    <DevAuthorisation>
       <Head>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
-      <TabHeader>Apps</TabHeader>
-      <AppGrid>
-        <TabCurrentlyWorkedOn />
-      </AppGrid>
-    </>
+      <TabHeader>Your Apps</TabHeader>
+      {!apps ? (
+        <AppGrid>
+          <NoAppsView hasApps={false}>No apps yet</NoAppsView>
+        </AppGrid>
+      ) : (
+        <DevAppsRows apps={apps} />
+      )}
+    </DevAuthorisation>
   );
 };
 DevApps.Layout = Layout;
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  res,
+}: GetServerSidePropsContext) => {
+  const uid = req?.headers.cookie?.toString().slice(4);
+  const config = { headers: { api_key: process.env.NEXT_PUBLIC_API_KEY } };
+  if (res && !uid) {
+    res.writeHead(302, { Location: "/login" });
+    res.end();
+    return { props: {} };
+  }
+  let apps: App[] = [];
+  let isDev: boolean = true;
+  try {
+    const apiRes = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/dev/getApps`,
+      { devId: uid },
+      config
+    );
+    apps = apiRes.data.apps;
+  } catch (err) {
+    if (res && err.response.data.err === "User is not a Developer.") {
+      isDev = false;
+    } else if (err.response.data.err === "No Apps found.") {
+      apps = undefined;
+    }
+  }
+
+  return {
+    props: {
+      apps,
+      hasUser: !!uid,
+      isDev,
+    },
+  };
+};
 
 export default DevApps;
