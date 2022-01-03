a93b5a05b3d7772de900221db01db1e93fc401df
Florian Kulig <florian.kulig@web.de>
2022-01-03 21:49:37 +0100

    iterating through dynamic data

--- src/components/dev/DevAppsRows.tsx
+++ src/components/dev/DevAppsRows.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { App } from "ts";
 
 import {
@@ -13,7 +13,6 @@ interface DevAppsRowsProps {
 }
 
 export const DevAppsRows: React.FC<DevAppsRowsProps> = ({ apps }) => {
-  const appId = "61cb55aa997627047a53a087";
   const [expandedApp, setExpandedApp] = useState<string>("");
 
   const handleExpand = (clickedAppId: string) => {
@@ -28,15 +27,19 @@ export const DevAppsRows: React.FC<DevAppsRowsProps> = ({ apps }) => {
     console.log(expandedApp);
   }, [expandedApp]);
   return (
-    <StyledAppDevRow>
-      <StyledAppDevRowHeader>
-        <h2 className="app-name">App Name</h2>
-        <ExpandButton
-          action={() => handleExpand(appId)}
-          isExpanded={expandedApp === appId}
-        />
-        <OptionsButton appId={appId} appName="Todoist" />
-      </StyledAppDevRowHeader>
-    </StyledAppDevRow>
+    <Fragment>
+      {apps?.map((app: App) => (
+        <StyledAppDevRow key={app._id}>
+          <StyledAppDevRowHeader>
+            <h2 className="app-name">{app.name}</h2>
+            <ExpandButton
+              action={() => handleExpand(app._id)}
+              isExpanded={expandedApp === app._id}
+            />
+            <OptionsButton appId={app._id} appName={app.name} />
+          </StyledAppDevRowHeader>
+        </StyledAppDevRow>
+      ))}
+    </Fragment>
   );
 };
