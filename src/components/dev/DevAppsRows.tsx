0a4a1fbb12771ecd45fc0d38e1d5f0e629a1bbe7
Florian Kulig <florian.kulig@web.de>
2022-01-03 21:40:34 +0100

    added expanded logic

--- src/components/dev/DevAppsRows.tsx
+++ src/components/dev/DevAppsRows.tsx
@@ -13,7 +13,7 @@ interface DevAppsRowsProps {
 }
 
 export const DevAppsRows: React.FC<DevAppsRowsProps> = ({ apps }) => {
-  const appId = "605b8651367f8f49f0c00caf";
+  const appId = "61cb55aa997627047a53a087";
   const [expandedApp, setExpandedApp] = useState<string>("");
 
   const handleExpand = (clickedAppId: string) => {
@@ -31,7 +31,10 @@ export const DevAppsRows: React.FC<DevAppsRowsProps> = ({ apps }) => {
     <StyledAppDevRow>
       <StyledAppDevRowHeader>
         <h2 className="app-name">App Name</h2>
-        <ExpandButton action={() => handleExpand(appId)} />
+        <ExpandButton
+          action={() => handleExpand(appId)}
+          isExpanded={expandedApp === appId}
+        />
         <OptionsButton appId={appId} appName="Todoist" />
       </StyledAppDevRowHeader>
     </StyledAppDevRow>
