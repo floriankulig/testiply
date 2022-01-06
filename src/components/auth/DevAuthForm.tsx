117674a879ff892f5ffd8eb63a30832aede520ba
Florian Kulig <florian.kulig@web.de>
2022-01-06 21:35:56 +0100

    added privacy policy to dev register

--- src/components/auth/DevAuthForm.tsx
+++ src/components/auth/DevAuthForm.tsx
@@ -192,7 +192,7 @@ export const DevAuthForm: React.FC<DevAuthFormProps> = ({
             <FormikCheckbox name="acceptedTAS">
               {/*LINK NEEDS TO POINT TO TERMS AND CONDITIONS LATER*/}I agree to
               the&nbsp;
-              <Link href="/register">
+              <Link href="https://api.testiply.n-mayr.net/privacy">
                 <span className="link">Terms and Conditions</span>
               </Link>
             </FormikCheckbox>
